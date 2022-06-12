49f4349cc4652ae10c98932ab77cfa06d09d5d9c
$MY_NAME <komuw05@gmail.com>
2022-06-12 19:44:04 +0300

    ff

--- server/rlimit.go
+++ server/rlimit.go
@@ -1,7 +1,7 @@
 package server
 
 import (
-	"fmt"
+	"log"
 
 	"golang.org/x/sys/unix" // syscall package is deprecated
 )
@@ -14,12 +14,18 @@ import (
 // 2. https://github.com/systemd/systemd/blob/e7901aba1480db21e06e21cef4f6486ad71b2ec5/src/basic/rlimit-util.c#L373
 // 3. https://github.com/golang/go/issues/46279
 func setRlimit() {
+	var err error
+	defer func() {
+		if err != nil {
+			log.Println("setRlimit err: ", err)
+		}
+	}()
+
 	var targetRlimit uint64 = 512_000 // value taken from link 1 above.
 	var currentRlimit unix.Rlimit
 	var newRlimit unix.Rlimit
 
-	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &currentRlimit)
-	_ = err
+	err = unix.Getrlimit(unix.RLIMIT_NOFILE, &currentRlimit)
 
 	newRlimit.Cur = currentRlimit.Cur
 	newRlimit.Max = currentRlimit.Max
@@ -30,9 +36,4 @@ func setRlimit() {
 	newRlimit.Cur = newRlimit.Max
 
 	err = unix.Setrlimit(unix.RLIMIT_NOFILE, &newRlimit)
-	_ = err
-}
-
-func printR(r unix.Rlimit) {
-	fmt.Printf("unix.Rlimit{Cur: %d, Max: %d}\n", r.Cur, r.Max)
 }
