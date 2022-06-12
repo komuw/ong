f421951991efd2c187ea2fb8fb4642e3a7cebe7f
$MY_NAME <komuw05@gmail.com>
2022-06-12 19:50:38 +0300

    ff

--- server/rlimit_test.go
+++ server/rlimit_test.go
@@ -1,7 +1,9 @@
 package server
 
 import (
+	"fmt"
 	"os"
+	"runtime"
 	"strings"
 	"testing"
 )
@@ -11,7 +13,10 @@ func Test_setRlimit(t *testing.T) {
 
 	maxFiles := 65_536 * 2 // most OSes set the soft limit at 1024, on ubuntu22.04 in github actions it is 65_536
 
+	fmt.Println("ffffffff", runtime.GOARCH, runtime.GOOS)
+
 	t.Run("rlimit reached", func(t *testing.T) {
+
 		var files []*os.File
 		var errs []error
 
