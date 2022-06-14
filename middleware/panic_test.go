c8e15c30d02f3adcda8a50c7b292bf65209d657c
$MY_NAME <komuw05@gmail.com>
2022-06-14 14:54:49 +0300

    m

---
+++ middleware/panic_test.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/akshayjshah/attest"
+)
+
+func handlerThatPanics(msg string, shouldPanic bool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		x := 3 + 9
+		_ = x
+		if shouldPanic {
+			panic(msg)
+		}
+		fmt.Fprint(w, msg)
+	}
+}
+
+func TestPanic(t *testing.T) {
+	t.Run("catches panics", func(t *testing.T) {
+		msg := "hello"
+		wrappedHandler := Panic(handlerThatPanics(msg, true))
+
+		rec := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/someUri", nil)
+		wrappedHandler.ServeHTTP(rec, r)
+
+		res := rec.Result()
+		defer res.Body.Close()
+
+		attest.Equal(t, res.StatusCode, http.StatusInternalServerError)
+	})
+
+	t.Run("ok if no panic", func(t *testing.T) {
+		msg := "hello"
+		wrappedHandler := Panic(handlerThatPanics(msg, false))
+
+		rec := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/someUri", nil)
+		wrappedHandler.ServeHTTP(rec, r)
+
+		res := rec.Result()
+		defer res.Body.Close()
+		attest.Equal(t, res.StatusCode, http.StatusOK)
+	})
+}
