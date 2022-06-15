e36c51ea60be0530b79aa686f832f21606c6586d
$MY_NAME <komuw05@gmail.com>
2022-06-15 20:22:51 +0300

    m

--- middleware/panic.go
+++ middleware/panic.go
@@ -6,6 +6,9 @@ import (
 	"runtime/debug"
 )
 
+// Most of the code here is insipired by:
+//   (a) https://github.com/eliben/code-for-blog license(Unlicense) can be found here: https://github.com/eliben/code-for-blog/blob/464a32f686d7646ba3fc612c19dbb550ec8a05b1/LICENSE
+
 // Panic is a middleware that recovers from panics in wrappedHandler.
 // It logs the stack trace and returns an InternalServerError response.
 func Panic(wrappedHandler http.HandlerFunc) http.HandlerFunc {
