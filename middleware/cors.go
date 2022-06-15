e7c161b13498441cf3d7b70bd88533153412ac09
$MY_NAME <komuw05@gmail.com>
2022-06-15 21:55:30 +0300

    m

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,6 +1,11 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+
+	"golang.org/x/exp/slices"
+)
 
 // Most of the code here is insipired by:
 //   (a) https://github.com/gofiber/fiber whose license(MIT) can be found here: https://github.com/rs/cors/blob/v1.8.2/LICENSE
@@ -20,6 +25,90 @@ import "net/http"
 // Cors is a middleware to implement Cross-Origin Resource Sharing support.
 func Cors(wrappedHandler http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		wrappedHandler(w, r)
+		if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+			// handle preflight request
+			handlePreflight(w, r)
+			// Preflight requests are standalone and should stop the chain as some other
+			// middleware may not handle OPTIONS requests correctly. One typical example
+			// is authentication middleware ; OPTIONS requests won't carry authentication headers.
+			w.WriteHeader(http.StatusNoContent)
+		} else {
+			// handle actual request
+			handleActualRequest(w, r)
+			wrappedHandler(w, r)
+		}
 	}
+}
+
+func handlePreflight(w http.ResponseWriter, r *http.Request) {
+	headers := w.Header()
+	origin := r.Header.Get("Origin")
+
+	if r.Method != http.MethodOptions {
+		// this is not a pre-flight request.
+		return
+	}
+
+	// Always set Vary headers
+	// see https://github.com/rs/cors/issues/10,
+	//     https://github.com/rs/cors/commit/dbdca4d95feaa7511a46e6f1efb3b3aa505bc43f#commitcomment-12352001
+	headers.Add("Vary", "Origin")
+	headers.Add("Vary", "Access-Control-Request-Method")
+	headers.Add("Vary", "Access-Control-Request-Headers")
+
+	if origin == "" {
+		return
+	}
+
+	if !isOriginAllowed(r, origin) {
+		return
+	}
+}
+
+type wildcard struct {
+	prefix string
+	suffix string
+}
+
+func (w wildcard) match(s string) bool {
+	return len(s) >= len(w.prefix)+len(w.suffix) && strings.HasPrefix(s, w.prefix) && strings.HasSuffix(s, w.suffix)
+}
+
+var (
+	allowedOrigins         = []string{}
+	allowedWildcardOrigins = []wildcard{}
+)
+
+func createWildcards() {
+	for _, origin := range allowedOrigins {
+		if i := strings.IndexByte(origin, '*'); i >= 0 {
+			// Split the origin in two: start and end string without the *
+			w := wildcard{origin[0:i], origin[i+1:]}
+			allowedWildcardOrigins = append(allowedWildcardOrigins, w)
+		}
+	}
+}
+
+func isOriginAllowed(r *http.Request, origin string) bool {
+	if slices.Contains(allowedOrigins, "*") {
+		return true
+	}
+
+	origin = strings.ToLower(origin)
+	for _, o := range allowedOrigins {
+		if o == origin {
+			return true
+		}
+	}
+
+	for _, w := range allowedWildcardOrigins {
+		if w.match(origin) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func handleActualRequest(w http.ResponseWriter, r *http.Request) {
 }
