f0d75505ed0b762171f31cb6e7d6e2500d616dea
$MY_NAME <komuw05@gmail.com>
2022-06-15 20:24:16 +0300

    m

--- middleware/cors.go
+++ middleware/cors.go
@@ -2,6 +2,9 @@ package middleware
 
 import "net/http"
 
+// Most of the code here is insipired by:
+//   (a) https://github.com/gofiber/fiber whose license(MIT) can be found here: https://github.com/rs/cors/blob/v1.8.2/LICENSE
+
 // Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to
 // indicate any origins (domain/scheme/port) other than its own from which a browser should permit loading resources.
 
