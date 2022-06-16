11d81879424ebb9e9257f24b837a77526f45e2f0
$MY_NAME <komuw05@gmail.com>
2022-06-16 16:03:59 +0300

    m

--- middleware/cors.go
+++ middleware/cors.go
@@ -7,6 +7,14 @@ import (
 	"golang.org/x/exp/slices"
 )
 
+const (
+	// header is used by browsers when issuing a preflight request.
+	acrmHeader = "Access-Control-Request-Method"
+	// used by browsers when issuing a preflight request to let the server know which HTTP headers the client might send when the actual request is made
+	acrhHeader   = "Access-Control-Request-Headers"
+	originHeader = "Origin"
+)
+
 // Most of the code here is insipired(or taken from) by:
 //   (a) https://github.com/gofiber/fiber whose license(MIT) can be found here: https://github.com/rs/cors/blob/v1.8.2/LICENSE
 
@@ -24,10 +32,31 @@ import (
 
 // Cors is a middleware to implement Cross-Origin Resource Sharing support.
 func Cors(wrappedHandler http.HandlerFunc) http.HandlerFunc {
+	// use `*` to allow all.
+	allowedOrigins := []string{} // TODO: offer ability for library user to provide these.
+	allowedWildcardOrigins := createWildcards(allowedOrigins)
+	_allowedMethods := []string{
+		// the spec by default allows this simple methods: GET, POST, HEAD.
+		http.MethodGet,
+		http.MethodPost,
+		http.MethodHead,
+	}
+	allowedMethods := []string{} // TODO: offer ability for library users to augument these.
+	for _, v := range _allowedMethods {
+		allowedMethods = append(allowedMethods, strings.ToUpper(v))
+	}
+
+	// use `*` to allow all.
+	_allowedHeaders := []string{}
+	allowedHeaders := []string{} // TODO: offer ability for library user to provide these.
+	for _, v := range _allowedHeaders {
+		allowedHeaders = append(allowedHeaders, http.CanonicalHeaderKey(v))
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+		if r.Method == http.MethodOptions && r.Header.Get(acrmHeader) != "" {
 			// handle preflight request
-			handlePreflight(w, r)
+			handlePreflight(w, r, allowedOrigins, allowedWildcardOrigins, allowedMethods, allowedHeaders)
 			// Preflight requests are standalone and should stop the chain as some other
 			// middleware may not handle OPTIONS requests correctly. One typical example
 			// is authentication middleware ; OPTIONS requests won't carry authentication headers.
@@ -40,9 +69,18 @@ func Cors(wrappedHandler http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
-func handlePreflight(w http.ResponseWriter, r *http.Request) {
+func handlePreflight(
+	w http.ResponseWriter,
+	r *http.Request,
+	allowedOrigins []string,
+	allowedWildcardOrigins []wildcard,
+	allowedMethods []string,
+	allowedHeaders []string,
+) {
 	headers := w.Header()
-	origin := r.Header.Get("Origin")
+	origin := r.Header.Get(originHeader)
+	reqMethod := r.Header.Get(acrmHeader)
+	reqHeader := r.Header.Get(acrhHeader)
 
 	if r.Method != http.MethodOptions {
 		// this is not a pre-flight request.
@@ -52,15 +90,23 @@ func handlePreflight(w http.ResponseWriter, r *http.Request) {
 	// Always set Vary headers
 	// see https://github.com/rs/cors/issues/10,
 	//     https://github.com/rs/cors/commit/dbdca4d95feaa7511a46e6f1efb3b3aa505bc43f#commitcomment-12352001
-	headers.Add("Vary", "Origin")
-	headers.Add("Vary", "Access-Control-Request-Method")
-	headers.Add("Vary", "Access-Control-Request-Headers")
+	headers.Add(varyHeader, originHeader)
+	headers.Add(varyHeader, acrmHeader)
+	headers.Add(varyHeader, acrhHeader)
 
 	if origin == "" {
 		return
 	}
 
-	if !isOriginAllowed(r, origin) {
+	if !isOriginAllowed(r, origin, allowedOrigins, allowedWildcardOrigins) {
+		return
+	}
+
+	if !isMethodAllowed(reqMethod, allowedMethods) {
+		return
+	}
+
+	if !areHeadersAllowed(reqHeader, allowedHeaders) {
 		return
 	}
 }
@@ -68,28 +114,40 @@ func handlePreflight(w http.ResponseWriter, r *http.Request) {
 type wildcard struct {
 	prefix string
 	suffix string
+	len    int
 }
 
 func (w wildcard) match(s string) bool {
-	return len(s) >= len(w.prefix)+len(w.suffix) && strings.HasPrefix(s, w.prefix) && strings.HasSuffix(s, w.suffix)
+	return len(s) >= w.len &&
+		strings.HasPrefix(s, w.prefix) &&
+		strings.HasSuffix(s, w.suffix)
 }
 
-var (
-	allowedOrigins         = []string{}
-	allowedWildcardOrigins = []wildcard{}
-)
-
-func createWildcards() {
+func createWildcards(allowedOrigins []string) []wildcard {
+	allowedWildcardOrigins := []wildcard{}
 	for _, origin := range allowedOrigins {
 		if i := strings.IndexByte(origin, '*'); i >= 0 {
 			// Split the origin in two: start and end string without the *
-			w := wildcard{origin[0:i], origin[i+1:]}
+			prefix := origin[0:i]
+			suffix := origin[i+1:]
+			w := wildcard{
+				prefix: prefix,
+				suffix: suffix,
+				len:    len(prefix) + len(suffix),
+			}
 			allowedWildcardOrigins = append(allowedWildcardOrigins, w)
 		}
 	}
+
+	return allowedWildcardOrigins
 }
 
-func isOriginAllowed(r *http.Request, origin string) bool {
+func isOriginAllowed(
+	r *http.Request,
+	origin string,
+	allowedOrigins []string,
+	allowedWildcardOrigins []wildcard,
+) bool {
 	if slices.Contains(allowedOrigins, "*") {
 		return true
 	}
@@ -108,6 +166,54 @@ func isOriginAllowed(r *http.Request, origin string) bool {
 	}
 
 	return false
+}
+
+func isMethodAllowed(method string, allowedMethods []string) bool {
+	if len(allowedMethods) == 0 {
+		// If no method allowed, always return false, even for preflight request
+		return false
+	}
+
+	method = strings.ToUpper(method)
+	if method == http.MethodOptions {
+		// Always allow preflight requests
+		return true
+	}
+
+	for _, m := range allowedMethods {
+		if m == method {
+			return true
+		}
+	}
+	return false
+}
+
+func areHeadersAllowed(reqHeader string, allowedHeaders []string) bool {
+	// Access-Control-Request-Headers: X-PINGOTHER, Content-Type
+	requestedHeaders := strings.Split(reqHeader, ",")
+
+	if len(requestedHeaders) == 0 {
+		return true
+	}
+
+	if slices.Contains(allowedHeaders, "*") {
+		return true
+	}
+
+	for _, header := range requestedHeaders {
+		header = http.CanonicalHeaderKey(header)
+		found := false
+		for _, h := range allowedHeaders {
+			if h == header {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
 }
 
 func handleActualRequest(w http.ResponseWriter, r *http.Request) {
--- middleware/cors_test.go
+++ middleware/cors_test.go
@@ -2,16 +2,39 @@ package middleware
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/akshayjshah/attest"
 )
 
-func TestCreateWildcards(t *testing.T) {
-	t.Run("TODO", func(t *testing.T) {
-		allowedOrigins = []string{"hello.com", "hi", "http://*.example.com"}
-		createWildcards()
-		fmt.Println("\n\t allowedWildcardOrigins: ", allowedWildcardOrigins)
+func someCorsHandler(msg string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, msg)
+	}
+}
 
-		// req := httptest.NewRequest(http.MethodGet, "/someUri", nil)
-		// isOriginAllowed(req, origin string)
+func TestCors(t *testing.T) {
+	t.Parallel()
+
+	t.Run("TODO", func(t *testing.T) {
+		t.Parallel()
+
+		msg := "hello"
+		wrappedHandler := Cors(someCorsHandler(msg))
+		rec := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, "/someUri", nil)
+		wrappedHandler.ServeHTTP(rec, req)
+
+		res := rec.Result()
+		defer res.Body.Close()
+
+		rb, err := io.ReadAll(res.Body)
+		attest.Ok(t, err)
+
+		attest.Equal(t, res.StatusCode, http.StatusOK)
+		attest.Equal(t, string(rb), msg)
 	})
 }
