e191c31048184ca8104b81bd39f81af6974844ff
$MY_NAME <komuw05@gmail.com>
2022-06-19 22:17:08 +0300

    m

--- middleware/gzip.go
+++ middleware/gzip.go
@@ -1,9 +1,11 @@
 package middleware
 
 import (
+	"compress/gzip"
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 )
 
 // Most of the code here is insipired by(or taken from):
@@ -27,8 +29,8 @@ func Gzip(wrappedHandler http.HandlerFunc) http.HandlerFunc {
 
 		gw := &responseWriter{
 			ResponseWriter: w,
-			h:              h,
-			buf:            bufferPool.Get().(*[]byte),
+			// h:              h,
+			buf: bufferPool.Get().(*[]byte),
 		}
 		defer func() {
 			err := gw.Close()
@@ -39,8 +41,60 @@ func Gzip(wrappedHandler http.HandlerFunc) http.HandlerFunc {
 
 		var modifiedRw http.ResponseWriter = gw
 
+		_, cok := w.(http.CloseNotifier)
+		_, hok := w.(http.Hijacker)
+		_, pok := w.(http.Pusher)
+
+		_ = cok
+		_ = hok
+		_ = pok
+
+		// TODO: implement this
+		// switch {
+		// case cok && hok:
+		// 	modifiedRw = closeNotifyHijackResponseWriter{gw}
+		// case cok && pok:
+		// 	modifiedRw = closeNotifyPusherResponseWriter{gw}
+		// case cok:
+		// 	modifiedRw = closeNotifyResponseWriter{gw}
+		// case hok:
+		// 	modifiedRw = hijackResponseWriter{gw}
+		// case pok:
+		// 	modifiedRw = pusherResponseWriter{gw}
+		// }
+
 		wrappedHandler(modifiedRw, r)
 	}
+}
+
+// TODO: copy/add docs.
+const defaultMinSize = 150
+
+var bufferPool = &sync.Pool{
+	New: func() interface{} {
+		buf := make([]byte, 0, defaultMinSize)
+		return &buf
+	},
+}
+
+// responseWriter provides an http.ResponseWriter interface,
+// which gzips bytes before writing them to the underlying
+// response. This doesn't close the writers, so don't forget
+// to do that. It can be configured to skip response smaller
+// than minSize.
+type responseWriter struct {
+	http.ResponseWriter
+
+	// h *handler
+
+	gw *gzip.Writer
+
+	// Holds the first part of the write before reaching
+	// the minSize or the end of the write.
+	buf *[]byte
+
+	// Saves the WriteHeader value.
+	code int
 }
 
 func shouldGzip(r *http.Request) bool {
