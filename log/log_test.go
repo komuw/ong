8415d34ee0ca5e8164ce9a3fd1f154da5a7ced8d
$MY_NAME <komuw05@gmail.com>
2022-06-21 17:28:49 +0300

    add error formatting

--- log/log_test.go
+++ log/log_test.go
@@ -3,6 +3,7 @@ package log
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"strings"
 	"testing"
 
@@ -55,5 +56,25 @@ func TestLogger(t *testing.T) {
 
 		id := getLogId(l.ctx)
 		attest.True(t, strings.Contains(w.String(), id))
+	})
+
+	t.Run("logs are rotated", func(t *testing.T) {
+		w := &bytes.Buffer{}
+		maxMsgs := 3
+		l := New(context.Background(), w, maxMsgs, true)
+
+		for i := 0; i <= (maxMsgs + 4); i++ {
+			infoMsg := "hello world" + " : " + fmt.Sprint(i)
+			l.Info(F{"what": infoMsg})
+		}
+		errMsg := "oops, Houston we got 99 problems."
+		l.Error(F{"err": errMsg})
+
+		attest.False(t, strings.Contains(w.String(), "hello world : 1"))
+		attest.False(t, strings.Contains(w.String(), "hello world : 2"))
+		attest.False(t, strings.Contains(w.String(), "hello world : 5"))
+		attest.True(t, strings.Contains(w.String(), "hello world : 6"))
+		attest.True(t, strings.Contains(w.String(), "hello world : 7"))
+		attest.True(t, strings.Contains(w.String(), errMsg))
 	})
 }
