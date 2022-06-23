4f0a0c731ad928266d0c40281e029befe5672b5f
$MY_NAME <komuw05@gmail.com>
2022-06-23 15:00:47 +0300

    m

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -8,101 +8,6 @@ import (
 	"net/http"
 )
 
-// Get is a middleware that only allows http GET requests and http OPTIONS requests.
-func Get(wrappedHandler http.HandlerFunc) http.HandlerFunc {
-	msg := "http method: %s not allowed. only allows http GET/OPTIONS"
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodOptions || r.Method == http.MethodGet {
-			// http OPTIONS is allowed because it is used for CORS(as a preflight request signal.)
-			wrappedHandler(w, r)
-		} else {
-			errMsg := fmt.Sprintf(msg, r.Method)
-			http.Error(
-				w,
-				errMsg,
-				http.StatusMethodNotAllowed,
-			)
-			return
-		}
-	}
-}
-
-// Post is a middleware that only allows http POST requests and http OPTIONS requests.
-func Post(wrappedHandler http.HandlerFunc) http.HandlerFunc {
-	msg := "http method: %s not allowed. only allows http POST/OPTIONS"
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodOptions || r.Method == http.MethodPost {
-			// http OPTIONS is allowed because it is used for CORS(as a preflight request signal.)
-			wrappedHandler(w, r)
-		} else {
-			errMsg := fmt.Sprintf(msg, r.Method)
-			http.Error(
-				w,
-				errMsg,
-				http.StatusMethodNotAllowed,
-			)
-			return
-		}
-	}
-}
-
-// Head is a middleware that only allows http HEAD requests and http OPTIONS requests.
-func Head(wrappedHandler http.HandlerFunc) http.HandlerFunc {
-	msg := "http method: %s not allowed. only allows http HEAD/OPTIONS"
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodOptions || r.Method == http.MethodHead {
-			// http OPTIONS is allowed because it is used for CORS(as a preflight request signal.)
-			wrappedHandler(w, r)
-		} else {
-			errMsg := fmt.Sprintf(msg, r.Method)
-			http.Error(
-				w,
-				errMsg,
-				http.StatusMethodNotAllowed,
-			)
-			return
-		}
-	}
-}
-
-// Put is a middleware that only allows http PUT requests and http OPTIONS requests.
-func Put(wrappedHandler http.HandlerFunc) http.HandlerFunc {
-	msg := "http method: %s not allowed. only allows http PUT/OPTIONS"
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodOptions || r.Method == http.MethodPut {
-			// http OPTIONS is allowed because it is used for CORS(as a preflight request signal.)
-			wrappedHandler(w, r)
-		} else {
-			errMsg := fmt.Sprintf(msg, r.Method)
-			http.Error(
-				w,
-				errMsg,
-				http.StatusMethodNotAllowed,
-			)
-			return
-		}
-	}
-}
-
-// Delete is a middleware that only allows http DELETE requests and http OPTIONS requests.
-func Delete(wrappedHandler http.HandlerFunc) http.HandlerFunc {
-	msg := "http method: %s not allowed. only allows http DELETE/OPTIONS"
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodOptions || r.Method == http.MethodDelete {
-			// http OPTIONS is allowed because it is used for CORS(as a preflight request signal.)
-			wrappedHandler(w, r)
-		} else {
-			errMsg := fmt.Sprintf(msg, r.Method)
-			http.Error(
-				w,
-				errMsg,
-				http.StatusMethodNotAllowed,
-			)
-			return
-		}
-	}
-}
-
 type opts struct {
 	domain             string
 	maxRequestsToReset int
@@ -112,13 +17,13 @@ type opts struct {
 	logOutput          io.Writer
 }
 
-/*
-  get(wh, "example.com", -1, nil, nil, nil, os.Stdout)
-*/
-func get(
+// all is a middleware that bundles all the core middlewares into one.
+// usage:
+//   all(wh, opts{"example.com", -1, nil, nil, nil, os.Stdout})
+func all(
 	wrappedHandler http.HandlerFunc,
 	o opts,
-) {
+) http.HandlerFunc {
 	domain := o.domain
 	maxRequestsToReset := o.maxRequestsToReset
 	allowedOrigins := o.allowedOrigins
@@ -139,7 +44,7 @@ func get(
 	//
 	// user -> panic -> log -> security -> cors -> csrf -> gzip -> actual-handler
 
-	Panic(
+	return Panic(
 		Log(
 			Security(
 				Cors(
@@ -161,4 +66,134 @@ func get(
 		),
 		logOutput,
 	)
+}
+
+// Get is a middleware that only allows http GET requests and http OPTIONS requests.
+func Get(wrappedHandler http.HandlerFunc, o opts) http.HandlerFunc {
+	return all(
+		get(wrappedHandler),
+		o,
+	)
+}
+
+func get(wrappedHandler http.HandlerFunc) http.HandlerFunc {
+	msg := "http method: %s not allowed. only allows http GET/OPTIONS"
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodOptions || r.Method == http.MethodGet {
+			// http OPTIONS is allowed because it is used for CORS(as a preflight request signal.)
+			wrappedHandler(w, r)
+		} else {
+			errMsg := fmt.Sprintf(msg, r.Method)
+			http.Error(
+				w,
+				errMsg,
+				http.StatusMethodNotAllowed,
+			)
+			return
+		}
+	}
+}
+
+// Post is a middleware that only allows http POST requests and http OPTIONS requests.
+func Post(wrappedHandler http.HandlerFunc, o opts) http.HandlerFunc {
+	return all(
+		post(wrappedHandler),
+		o,
+	)
+}
+
+func post(wrappedHandler http.HandlerFunc) http.HandlerFunc {
+	msg := "http method: %s not allowed. only allows http POST/OPTIONS"
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodOptions || r.Method == http.MethodPost {
+			// http OPTIONS is allowed because it is used for CORS(as a preflight request signal.)
+			wrappedHandler(w, r)
+		} else {
+			errMsg := fmt.Sprintf(msg, r.Method)
+			http.Error(
+				w,
+				errMsg,
+				http.StatusMethodNotAllowed,
+			)
+			return
+		}
+	}
+}
+
+// Head is a middleware that only allows http HEAD requests and http OPTIONS requests.
+func Head(wrappedHandler http.HandlerFunc, o opts) http.HandlerFunc {
+	return all(
+		head(wrappedHandler),
+		o,
+	)
+}
+
+func head(wrappedHandler http.HandlerFunc) http.HandlerFunc {
+	msg := "http method: %s not allowed. only allows http HEAD/OPTIONS"
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodOptions || r.Method == http.MethodHead {
+			// http OPTIONS is allowed because it is used for CORS(as a preflight request signal.)
+			wrappedHandler(w, r)
+		} else {
+			errMsg := fmt.Sprintf(msg, r.Method)
+			http.Error(
+				w,
+				errMsg,
+				http.StatusMethodNotAllowed,
+			)
+			return
+		}
+	}
+}
+
+// Put is a middleware that only allows http PUT requests and http OPTIONS requests.
+func Put(wrappedHandler http.HandlerFunc, o opts) http.HandlerFunc {
+	return all(
+		head(wrappedHandler),
+		o,
+	)
+}
+
+func put(wrappedHandler http.HandlerFunc) http.HandlerFunc {
+	msg := "http method: %s not allowed. only allows http PUT/OPTIONS"
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodOptions || r.Method == http.MethodPut {
+			// http OPTIONS is allowed because it is used for CORS(as a preflight request signal.)
+			wrappedHandler(w, r)
+		} else {
+			errMsg := fmt.Sprintf(msg, r.Method)
+			http.Error(
+				w,
+				errMsg,
+				http.StatusMethodNotAllowed,
+			)
+			return
+		}
+	}
+}
+
+// Delete is a middleware that only allows http DELETE requests and http OPTIONS requests.
+func Delete(wrappedHandler http.HandlerFunc, o opts) http.HandlerFunc {
+	return all(
+		head(wrappedHandler),
+		o,
+	)
+}
+
+func delete(wrappedHandler http.HandlerFunc) http.HandlerFunc {
+	msg := "http method: %s not allowed. only allows http DELETE/OPTIONS"
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodOptions || r.Method == http.MethodDelete {
+			// http OPTIONS is allowed because it is used for CORS(as a preflight request signal.)
+			wrappedHandler(w, r)
+		} else {
+			errMsg := fmt.Sprintf(msg, r.Method)
+			http.Error(
+				w,
+				errMsg,
+				http.StatusMethodNotAllowed,
+			)
+			return
+		}
+	}
 }
