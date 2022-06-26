bb65a56f07a36a1e96abf32f0754b46358349454
$MY_NAME <komuw05@gmail.com>
2022-06-26 15:38:41 +0300

    i

--- middleware/ratelimiter.go
+++ middleware/ratelimiter.go
@@ -3,31 +3,60 @@ package middleware
 import (
 	"fmt"
 	"net/http"
-	"strings"
+	"time"
 )
+
+type latency struct {
+	duration time.Duration
+	at       time.Time
+}
+
+func (l latency) String() string {
+	return fmt.Sprintf("{dur: %s, at: %s}", l.duration, l.at)
+}
+
+type latencyQueue []latency
+
+// TODO: with the algorithm we are gong with; this looks like a loadShedder rather than a rateLimiter.
 
 // RateLimiter is a middleware that rate limits requests based on the IP address.
 func RateLimiter(wrappedHandler http.HandlerFunc) http.HandlerFunc {
+	lq := latencyQueue{} // TODO, we need to purge this queue regurlary
+
+	// The minimum number of past requests that have to be available, in the last `samplingPeriod` seconds for us to make a decision.
+	// If there were fewer requests in the `samplingPeriod`, then we do decide to let things continue without ratelimiting.
+	minSampleSize := 10
+	samplingPeriod := 10 * time.Second
+	// The p99 latency(in milliSeconds) at which point we start dropping requests.
+	breach_latency := 3 * time.Second
+
+	_ = minSampleSize
+	_ = samplingPeriod
+	_ = breach_latency // TODO: remove this.
+
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("r.RemoteAddr: ", r.RemoteAddr)
-		fmt.Println("r.headers: ", r.Header)
-		fmt.Println("r.Method: ", r.Method)
-		ip := fetchIP(r.RemoteAddr)
-		fmt.Println("ip: ", ip)
+		startReq := time.Now().UTC()
+		defer func() {
+			endReq := time.Now().UTC()
+			durReq := endReq.Sub(startReq)
+			lq = append(lq, latency{duration: durReq, at: endReq})
+
+			fmt.Println("\n\t lq: ", lq)
+		}()
 
 		wrappedHandler(w, r)
 	}
 }
 
-func fetchIP(remoteAddr string) string {
-	// the documentation of `http.Request.RemoteAddr` says:
-	// RemoteAddr is not filled in by ReadRequest and has no defined format.
-	// So we cant rely on it been present, or having a given format.
-	// Although, net/http makes a good effort of availing it & in a standard format.
-	//
-	ipAddr := strings.Split(remoteAddr, ":")
-	return ipAddr[0]
-}
+// func fetchIP(remoteAddr string) string {
+// 	// the documentation of `http.Request.RemoteAddr` says:
+// 	// RemoteAddr is not filled in by ReadRequest and has no defined format.
+// 	// So we cant rely on it been present, or having a given format.
+// 	// Although, net/http makes a good effort of availing it & in a standard format.
+// 	//
+// 	ipAddr := strings.Split(remoteAddr, ":")
+// 	return ipAddr[0]
+// }
 
 // TODO:
 //
--- middleware/ratelimiter_test.go
+++ middleware/ratelimiter_test.go
@@ -6,16 +6,20 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/akshayjshah/attest"
 )
 
+// TODO: rename this to loadShedder?
 func someRateLimiterHandler(msg string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(200 * time.Millisecond)
 		fmt.Fprint(w, msg)
 	}
 }
 
+// TODO: rename this to loadShedder?
 func TestRateLimiter(t *testing.T) {
 	t.Parallel()
 
@@ -39,24 +43,44 @@ func TestRateLimiter(t *testing.T) {
 		attest.Equal(t, string(rb), msg)
 	})
 
-	t.Run("bad remoteAddr", func(t *testing.T) {
-		t.Parallel()
+	// t.Run("success", func(t *testing.T) {
+	// 	t.Parallel()
 
-		msg := "hello"
-		wrappedHandler := RateLimiter(someRateLimiterHandler(msg))
+	// 	msg := "hello"
+	// 	wrappedHandler := RateLimiter(someRateLimiterHandler(msg))
 
-		rec := httptest.NewRecorder()
-		req := httptest.NewRequest(http.MethodGet, "/someUri", nil)
-		req.RemoteAddr = "BadRemoteAddr"
-		wrappedHandler.ServeHTTP(rec, req)
+	// 	rec := httptest.NewRecorder()
+	// 	req := httptest.NewRequest(http.MethodGet, "/someUri", nil)
+	// 	wrappedHandler.ServeHTTP(rec, req)
 
-		res := rec.Result()
-		defer res.Body.Close()
+	// 	res := rec.Result()
+	// 	defer res.Body.Close()
 
-		rb, err := io.ReadAll(res.Body)
-		attest.Ok(t, err)
+	// 	rb, err := io.ReadAll(res.Body)
+	// 	attest.Ok(t, err)
 
-		attest.Equal(t, res.StatusCode, http.StatusOK)
-		attest.Equal(t, string(rb), msg)
-	})
+	// 	attest.Equal(t, res.StatusCode, http.StatusOK)
+	// 	attest.Equal(t, string(rb), msg)
+	// })
+
+	// t.Run("bad remoteAddr", func(t *testing.T) {
+	// 	t.Parallel()
+
+	// 	msg := "hello"
+	// 	wrappedHandler := RateLimiter(someRateLimiterHandler(msg))
+
+	// 	rec := httptest.NewRecorder()
+	// 	req := httptest.NewRequest(http.MethodGet, "/someUri", nil)
+	// 	req.RemoteAddr = "BadRemoteAddr"
+	// 	wrappedHandler.ServeHTTP(rec, req)
+
+	// 	res := rec.Result()
+	// 	defer res.Body.Close()
+
+	// 	rb, err := io.ReadAll(res.Body)
+	// 	attest.Ok(t, err)
+
+	// 	attest.Equal(t, res.StatusCode, http.StatusOK)
+	// 	attest.Equal(t, string(rb), msg)
+	// })
 }
