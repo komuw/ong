bf3c2899439b4271add9abcc58ee9d665d34ed4b
$MY_NAME <komuw05@gmail.com>
2022-06-26 20:19:29 +0300

    g

--- middleware/loadshed_test.go
+++ middleware/loadshed_test.go
@@ -95,47 +95,6 @@ func TestLoadShedder(t *testing.T) {
 		}
 		wg.Wait()
 	})
-
-	// t.Run("success", func(t *testing.T) {
-	// 	t.Parallel()
-
-	// 	msg := "hello"
-	// 	wrappedHandler := LoadShedder(someLoadShedderHandler(msg))
-
-	// 	rec := httptest.NewRecorder()
-	// 	req := httptest.NewRequest(http.MethodGet, "/someUri", nil)
-	// 	wrappedHandler.ServeHTTP(rec, req)
-
-	// 	res := rec.Result()
-	// 	defer res.Body.Close()
-
-	// 	rb, err := io.ReadAll(res.Body)
-	// 	attest.Ok(t, err)
-
-	// 	attest.Equal(t, res.StatusCode, http.StatusOK)
-	// 	attest.Equal(t, string(rb), msg)
-	// })
-
-	// t.Run("bad remoteAddr", func(t *testing.T) {
-	// 	t.Parallel()
-
-	// 	msg := "hello"
-	// 	wrappedHandler := LoadShedder(someLoadShedderHandler(msg))
-
-	// 	rec := httptest.NewRecorder()
-	// 	req := httptest.NewRequest(http.MethodGet, "/someUri", nil)
-	// 	req.RemoteAddr = "BadRemoteAddr"
-	// 	wrappedHandler.ServeHTTP(rec, req)
-
-	// 	res := rec.Result()
-	// 	defer res.Body.Close()
-
-	// 	rb, err := io.ReadAll(res.Body)
-	// 	attest.Ok(t, err)
-
-	// 	attest.Equal(t, res.StatusCode, http.StatusOK)
-	// 	attest.Equal(t, string(rb), msg)
-	// })
 }
 
 func TestPercentile(t *testing.T) {
