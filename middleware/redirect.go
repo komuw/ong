b82b95a6172a70461da8114042110ef2c3ff0816
$MY_NAME <komuw05@gmail.com>
2022-06-28 22:41:48 +0300

    g

--- middleware/redirect.go
+++ middleware/redirect.go
@@ -3,19 +3,47 @@ package middleware
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 // HttpsRedirector is a middleware that redirects http requests to https.
 func HttpsRedirector(wrappedHandler http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.TLS == nil {
-			// r.URL.Scheme = "https"
-			path := fmt.Sprintf("https://%s%s", r.Host, r.URL.String())
-			fmt.Println(path, r.Host, r.URL.String())
+		isTls := strings.EqualFold(r.URL.Scheme, "https") || r.TLS != nil
+
+		if !isTls {
+			url := r.URL
+			url.Scheme = "https"
+			url.Host = r.Host
+
+			path := url.String() // fmt.Sprintf("https://%s%s", r.Host, r.URL.String())
+			fmt.Println("\t HttpsRedirector: ", path)
 			http.Redirect(w, r, path, http.StatusPermanentRedirect)
 			return
 		}
 
 		wrappedHandler(w, r)
 	}
+}
+
+func RedirectToHTTPSRouter(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("\n\t RedirectToHTTPSRouter called..\n.")
+		isTls := strings.EqualFold(r.URL.Scheme, "https") || r.TLS != nil
+
+		if !isTls {
+			// kama.Dirp(r.URL)
+			// r.URL.Scheme = "https"
+			url := r.URL
+			url.Scheme = "https"
+			url.Host = r.Host
+
+			path := url.String() // fmt.Sprintf("https://%s%s", r.Host, r.URL.String())
+			fmt.Println(path)    // r.Host, r.URL.String())
+			http.Redirect(w, r, path, http.StatusPermanentRedirect)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
