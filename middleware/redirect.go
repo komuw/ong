aecad8587d67b79886f325786e4317f393a76605
$MY_NAME <komuw05@gmail.com>
2022-06-28 16:31:58 +0300

    g

---
+++ middleware/redirect.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"io"
+	"net/http"
+)
+
+// HttpsRedirector is a middleware that redirects http requests to https.
+func HttpsRedirector(wrappedHandler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.TLS == nil {
+			// http.RedirectHandler(url string, code int)
+			http.Redirect(w, r, r.URL.Host, http.StatusPermanentRedirect)
+			return
+		}
+
+		wrappedHandler(w, r)
+	}
+}
