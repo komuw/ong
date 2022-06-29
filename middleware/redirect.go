aae5258cd36bf15bf993123f0b33e56c9236935c
$MY_NAME <komuw05@gmail.com>
2022-06-29 10:16:33 +0300

    g

--- middleware/redirect.go
+++ middleware/redirect.go
@@ -45,34 +45,3 @@ func HttpsRedirector(httpsPort string) http.HandlerFunc {
 		)
 	}
 }
-
-func RedirectToHTTPSRouter() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("\n\t RedirectToHTTPSRouter called..\n.")
-		isTls := strings.EqualFold(r.URL.Scheme, "https") || r.TLS != nil
-
-		if !isTls {
-			// kama.Dirp(r.URL)
-			// r.URL.Scheme = "https"
-
-			fmt.Println(" r.Host: ", r.Host)
-			url := r.URL
-			url.Scheme = "https"
-
-			host, _, err := net.SplitHostPort(r.Host)
-			if err != nil {
-				host = r.Host
-			}
-			httpsPort := "8081"
-			host = net.JoinHostPort(host, httpsPort)
-			url.Host = host
-
-			path := url.String() // fmt.Sprintf("https://%s%s", r.Host, r.URL.String())
-			fmt.Println(path)    // r.Host, r.URL.String())
-			http.Redirect(w, r, path, http.StatusPermanentRedirect)
-			return
-		}
-
-		// next.ServeHTTP(w, r)
-	})
-}
