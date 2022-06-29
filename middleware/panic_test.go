a6e31d63a57c721e627ebc63cf1faebe31d45445
$MY_NAME <komuw05@gmail.com>
2022-06-29 23:00:26 +0300

    hold

--- middleware/panic_test.go
+++ middleware/panic_test.go
@@ -106,4 +106,35 @@ func TestPanic(t *testing.T) {
 		}
 		attest.True(t, strings.Contains(logOutput.String(), "stack"))
 	})
+
+	// t.Run("concurrency safe", func(t *testing.T) {
+	// 	t.Parallel()
+
+	// 	logOutput := &bytes.Buffer{}
+	// 	msg := "hello"
+	// 	// for this concurrency test, we have to re-use the same wrappedHandler
+	// 	// so that state is shared and thus we can see if there is any state which is not handled correctly.
+	// 	wrappedHandler := Panic(handlerThatPanics(msg, true, nil), logOutput)
+
+	// 	runhandler := func() {
+	// 		rec := httptest.NewRecorder()
+	// 		req := httptest.NewRequest(http.MethodGet, "/someUri", nil)
+	// 		wrappedHandler.ServeHTTP(rec, req)
+
+	// 		res := rec.Result()
+	// 		defer res.Body.Close()
+
+	// 		attest.Equal(t, res.StatusCode, http.StatusInternalServerError)
+	// 	}
+
+	// 	wg := &sync.WaitGroup{}
+	// 	for rN := 0; rN <= 10; rN++ {
+	// 		wg.Add(1)
+	// 		go func() {
+	// 			defer wg.Done()
+	// 			runhandler()
+	// 		}()
+	// 	}
+	// 	wg.Wait()
+	// })
 }
