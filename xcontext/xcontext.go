8ab0c53166df23f7184f75fbc11d48ab78f29840
Komu Wairagu <komuw05@gmail.com>
2022-09-06 22:12:01 +0300

    Add an xcontext package (#109)
    What:
    - Add an xcontext package
    
    Why:
    - Fixes: https://github.com/komuw/ong/issues/108

---
+++ xcontext/xcontext.go
@@ -0,0 +1,27 @@
+// Package xcontext is a package to offer the extra functionality we need
+// from contexts that is not available from the standard context package.
+package xcontext
+
+import (
+	"context"
+	"time"
+)
+
+// Most of the code here is insipired by(or taken from):
+//   (a) https://github.com/golang/pkgsite whose license(BSD 3-Clause "New") can be found here: https://github.com/golang/pkgsite/blob/24f94ffc546bde6aae0552efa6a940041d9d28e1/LICENSE
+//
+
+// Note: There's a Go proposal to bring something like into the std lib:
+//       https://github.com/golang/go/issues/40221
+//
+
+// Detach returns a context that keeps all the values of its parent context
+// but detaches from the cancellation and error handling.
+func Detach(ctx context.Context) context.Context { return detachedContext{ctx} }
+
+type detachedContext struct{ parent context.Context }
+
+func (v detachedContext) Deadline() (time.Time, bool)       { return time.Time{}, false }
+func (v detachedContext) Done() <-chan struct{}             { return nil }
+func (v detachedContext) Err() error                        { return nil }
+func (v detachedContext) Value(key interface{}) interface{} { return v.parent.Value(key) }
---
+++ xcontext/xcontext_test.go
@@ -0,0 +1,49 @@
+package xcontext
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+type ctxKey string
+
+func TestDetach(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	defer cancel()
+
+	key := ctxKey("key")
+	ctx = context.WithValue(ctx, key, "value")
+	dctx := Detach(ctx)
+	// Detached context has the same values.
+	got, ok := dctx.Value(key).(string)
+	if !ok || got != "value" {
+		t.Errorf("Value: got (%v, %t), want 'value', true", got, ok)
+	}
+	// Detached context doesn't time out.
+	time.Sleep(500 * time.Millisecond)
+	if err := ctx.Err(); err != context.DeadlineExceeded {
+		t.Fatalf("original context Err: got %v, want DeadlineExceeded", err)
+	}
+	if err := dctx.Err(); err != nil {
+		t.Errorf("detached context Err: got %v, want nil", err)
+	}
+}
+
+func ExampleDetach() {
+	someFunc := func(ctx context.Context) {}
+	someOtherFunc := func(ctx context.Context) {}
+
+	foo := func() {
+		key := ctxKey("key")
+		ctx := context.WithValue(context.Background(), key, "my_value")
+
+		// Detach is not required here.
+		someFunc(ctx)
+
+		// We need Detach here, because someOtherFunc(having been called in a goroutine) can outlive the cancellation of the parent context.
+		go someOtherFunc(Detach(ctx))
+	}
+
+	foo()
+}
