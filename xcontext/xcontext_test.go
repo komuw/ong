896defe84fecdc632d75c242544eaa34667b0774
$MY_NAME <komuw05@gmail.com>
2022-09-06 21:51:55 +0300

    f

---
+++ xcontext/xcontext_test.go
@@ -0,0 +1,31 @@
+package xcontext
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+type ctxKey string
+
+var key = ctxKey("key")
+
+func TestDetach(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	defer cancel()
+	ctx = context.WithValue(ctx, key, "value")
+	dctx := Detach(ctx)
+	// Detached context has the same values.
+	got, ok := dctx.Value(key).(string)
+	if !ok || got != "value" {
+		t.Errorf("Value: got (%v, %t), want 'value', true", got, ok)
+	}
+	// Detached context doesn't time out.
+	time.Sleep(500 * time.Millisecond)
+	if err := ctx.Err(); err != context.DeadlineExceeded {
+		t.Fatalf("original context Err: got %v, want DeadlineExceeded", err)
+	}
+	if err := dctx.Err(); err != nil {
+		t.Errorf("detached context Err: got %v, want nil", err)
+	}
+}
