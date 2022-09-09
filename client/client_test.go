1a55a0a6284d65fa88cb5d86318c1e61f712960a
$MY_NAME <komuw05@gmail.com>
2022-09-09 19:12:28 +0300

    s

--- client/client_test.go
+++ client/client_test.go
@@ -1,42 +1,59 @@
 package client
 
 import (
-	"fmt"
+	"net/http"
 	"testing"
+
+	"github.com/akshayjshah/attest"
 )
 
 func TestClient(t *testing.T) {
 	t.Parallel()
 
+	// aws metadata api.
+	urlsInPrivate := []string{
+		"http://[fd00:ec2::254]/latest/meta-data/",
+		"http://169.254.169.254/latest/meta-data/",
+	}
+
+	urlsInPublic := []string{
+		"http://www.example.com",
+		"https://www.example.com",
+	}
+
 	t.Run("ssrf safe", func(t *testing.T) {
 		t.Parallel()
 
-		// http://[fd00:ec2::254]/latest/meta-data/
-		url := "http://[fd00:ec2::254]/latest/meta-data/" //"http://169.254.169.254/latest/meta-data/" //"https://www.google.com"
-
 		ssrfSafe := true
-		cli := getClient(ssrfSafe)
 
-		res, err := cli.Get(url)
-		if err != nil {
-			t.Fatal(err)
+		for _, url := range urlsInPrivate {
+			cli := getClient(ssrfSafe)
+			_, err := cli.Get(url)
+			attest.Error(t, err)
+			attest.Subsequence(t, err.Error(), "is not a public IP address")
 		}
-		fmt.Println("\n\t res, ssrfSafe: ", res.StatusCode, ssrfSafe)
+
+		for _, url := range urlsInPublic {
+			cli := getClient(ssrfSafe)
+			res, err := cli.Get(url)
+			attest.Ok(t, err)
+			attest.Equal(t, res.StatusCode, http.StatusOK)
+		}
 	})
 
-	t.Run("ssrf unsafe", func(t *testing.T) {
-		t.Parallel()
+	// t.Run("ssrf unsafe", func(t *testing.T) {
+	// 	t.Parallel()
 
-		// http://[fd00:ec2::254]/latest/meta-data/
-		url := "http://[fd00:ec2::254]/latest/meta-data/" //"http://169.254.169.254/latest/meta-data/" //"https://www.google.com"
+	// 	// http://[fd00:ec2::254]/latest/meta-data/
+	// 	url := "http://[fd00:ec2::254]/latest/meta-data/" //"http://169.254.169.254/latest/meta-data/" //"https://www.google.com"
 
-		ssrfSafe := false
-		cli := getClient(ssrfSafe)
+	// 	ssrfSafe := false
+	// 	cli := getClient(ssrfSafe)
 
-		res, err := cli.Get(url)
-		if err != nil {
-			t.Fatal(err)
-		}
-		fmt.Println("\n\t res, ssrfSafe: ", res.StatusCode, ssrfSafe)
-	})
+	// 	res, err := cli.Get(url)
+	// 	if err != nil {
+	// 		t.Fatal(err)
+	// 	}
+	// 	fmt.Println("\n\t res, ssrfSafe: ", res.StatusCode, ssrfSafe)
+	// })
 }
