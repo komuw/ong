efdd677f363627c7f6ad4aedb53b0af916dc1a46
$MY_NAME <komuw05@gmail.com>
2022-09-13 15:38:33 +0300

    f

--- enc/multi_enc_test.go
+++ enc/multi_enc_test.go
@@ -1,6 +1,7 @@
 package enc
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/akshayjshah/attest"
@@ -42,5 +43,31 @@ func TestMultiEnc(t *testing.T) {
 		attest.Ok(t, err)
 
 		attest.Equal(t, string(decryptedMsg), msgToEncryt)
+	})
+
+	t.Run("concurrency safe", func(t *testing.T) {
+		t.Parallel()
+
+		msgToEncryt := "hello world!"
+
+		run := func() {
+			key1, key2 := getMultiSecretKeys()
+			enc := NewMulti(key1, key2)
+
+			encryptedMsg1, encryptedMsg2 := enc.encrypt(msgToEncryt)
+			decryptedMsg, err := enc.decrypt(encryptedMsg1, encryptedMsg2)
+			attest.Ok(t, err)
+			attest.Equal(t, string(decryptedMsg), msgToEncryt)
+		}
+
+		wg := &sync.WaitGroup{}
+		for rN := 0; rN <= 7; rN++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				run()
+			}()
+		}
+		wg.Wait()
 	})
 }
