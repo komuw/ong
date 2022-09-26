89f9c89db4a4f5f599cfd227d5597c74dd6da60c
$MY_NAME <komuw05@gmail.com>
2022-09-26 03:59:12 +0300

    f

--- cry/hash.go
+++ cry/hash.go
@@ -1,10 +1,16 @@
 package cry
 
 import (
+	"errors"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"golang.org/x/crypto/scrypt"
 )
+
+// Most of the code here is insipired by(or taken from):
+//   (a) https://github.com/elithrar/simple-scrypt whose license(MIT) can be found here: https://github.com/elithrar/simple-scrypt/blob/v1.3.0/LICENSE
 
 const (
 	version = 1
@@ -34,4 +40,39 @@ func hash(password string) string {
 		salt,
 		derivedKey,
 	)
+}
+
+func eql(password, hash string) error {
+	params := strings.Split(hash, "$")
+
+	if len(params) != 6 {
+		return errors.New("unable to parse")
+	}
+
+	pv, err := strconv.Atoi(params[0])
+	if err != nil {
+		return err
+	}
+	if pv != version {
+		// TODO: better error messages
+		return errors.New("unable to parse")
+	}
+
+	pn, err := strconv.Atoi(params[1])
+	if err != nil {
+		return err
+	}
+
+	pr, err := strconv.Atoi(params[2])
+	if err != nil {
+		return err
+	}
+
+	pp, err := strconv.Atoi(params[3])
+	if err != nil {
+		return err
+	}
+
+	pSalt := params[4]
+	pDerivedKey := params[5]
 }
