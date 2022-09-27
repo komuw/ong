cf65e5f5f3009797fe3b5de867d37acd8742ec80
$MY_NAME <komuw05@gmail.com>
2022-09-27 11:48:29 +0300

    add t.parallel and name tests

--- server/newmux/mux_test.go
+++ server/newmux/mux_test.go
@@ -2,6 +2,7 @@ package mux
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -18,111 +19,209 @@ var tests = []struct {
 }{
 	// simple path matching
 	{
-		"GET", "/one",
-		"GET", "/one", true, nil,
+		"GET",
+		"/one",
+		"GET",
+		"/one",
+		true,
+		nil,
 	},
 	{
-		"GET", "/two",
-		"GET", "/two", true, nil,
+		"GET",
+		"/two",
+		"GET",
+		"/two",
+		true,
+		nil,
 	},
 	{
-		"GET", "/three",
-		"GET", "/three", true, nil,
+		"GET",
+		"/three",
+		"GET",
+		"/three",
+		true,
+		nil,
 	},
 	// methods
 	{
-		"get", "/methodcase",
-		"GET", "/methodcase", true, nil,
+		"get",
+		"/methodcase",
+		"GET",
+		"/methodcase",
+		true,
+		nil,
 	},
 	{
-		"Get", "/methodcase",
-		"get", "/methodcase", true, nil,
+		"Get",
+		"/methodcase",
+		"get",
+		"/methodcase",
+		true,
+		nil,
 	},
 	{
-		"GET", "/methodcase",
-		"get", "/methodcase", true, nil,
+		"GET",
+		"/methodcase",
+		"get",
+		"/methodcase",
+		true,
+		nil,
 	},
 	{
-		"GET", "/method1",
-		"POST", "/method1", false, nil,
+		"GET",
+		"/method1",
+		"POST",
+		"/method1",
+		false,
+		nil,
 	},
 	{
-		"DELETE", "/method2",
-		"GET", "/method2", false, nil,
+		"DELETE",
+		"/method2",
+		"GET",
+		"/method2",
+		false,
+		nil,
 	},
 	{
-		"GET", "/method3",
-		"PUT", "/method3", false, nil,
+		"GET",
+		"/method3",
+		"PUT",
+		"/method3",
+		false,
+		nil,
 	},
 	// all methods
 	{
-		"*", "/all-methods",
-		"GET", "/all-methods", true, nil,
+		"*",
+		"/all-methods",
+		"GET",
+		"/all-methods",
+		true,
+		nil,
 	},
 	{
-		"*", "/all-methods",
-		"POST", "/all-methods", true, nil,
+		"*",
+		"/all-methods",
+		"POST",
+		"/all-methods",
+		true,
+		nil,
 	},
 	{
-		"*", "/all-methods",
-		"PUT", "/all-methods", true, nil,
+		"*",
+		"/all-methods",
+		"PUT",
+		"/all-methods",
+		true,
+		nil,
 	},
 	// nested
 	{
-		"GET", "/parent/child/one",
-		"GET", "/parent/child/one", true, nil,
+		"GET",
+		"/parent/child/one",
+		"GET",
+		"/parent/child/one",
+		true,
+		nil,
 	},
 	{
-		"GET", "/parent/child/two",
-		"GET", "/parent/child/two", true, nil,
+		"GET",
+		"/parent/child/two",
+		"GET",
+		"/parent/child/two",
+		true,
+		nil,
 	},
 	{
-		"GET", "/parent/child/three",
-		"GET", "/parent/child/three", true, nil,
+		"GET",
+		"/parent/child/three",
+		"GET",
+		"/parent/child/three",
+		true,
+		nil,
 	},
 	// slashes
 	{
-		"GET", "slashes/one",
-		"GET", "/slashes/one", true, nil,
+		"GET",
+		"slashes/one",
+		"GET",
+		"/slashes/one",
+		true,
+		nil,
 	},
 	{
-		"GET", "/slashes/two",
-		"GET", "slashes/two", true, nil,
+		"GET",
+		"/slashes/two",
+		"GET",
+		"slashes/two",
+		true,
+		nil,
 	},
 	{
-		"GET", "slashes/three/",
-		"GET", "/slashes/three", true, nil,
+		"GET",
+		"slashes/three/",
+		"GET",
+		"/slashes/three",
+		true,
+		nil,
 	},
 	{
-		"GET", "/slashes/four",
-		"GET", "slashes/four/", true, nil,
+		"GET",
+		"/slashes/four",
+		"GET",
+		"slashes/four/",
+		true,
+		nil,
 	},
 	// prefix
 	{
-		"GET", "/prefix/",
-		"GET", "/prefix/anything/else", true, nil,
+		"GET",
+		"/prefix/",
+		"GET",
+		"/prefix/anything/else",
+		true,
+		nil,
 	},
 	{
-		"GET", "/not-prefix",
-		"GET", "/not-prefix/anything/else", false, nil,
+		"GET",
+		"/not-prefix",
+		"GET",
+		"/not-prefix/anything/else",
+		false,
+		nil,
 	},
 	{
-		"GET", "/prefixdots...",
-		"GET", "/prefixdots/anything/else", true, nil,
+		"GET",
+		"/prefixdots...",
+		"GET",
+		"/prefixdots/anything/else",
+		true,
+		nil,
 	},
 	{
-		"GET", "/prefixdots...",
-		"GET", "/prefixdots", true, nil,
+		"GET",
+		"/prefixdots...",
+		"GET",
+		"/prefixdots",
+		true,
+		nil,
 	},
 	// path params
 	{
-		"GET", "/path-param/:id",
-		"GET", "/path-param/123", true,
+		"GET",
+		"/path-param/:id",
+		"GET",
+		"/path-param/123",
+		true,
 		map[string]string{"id": "123"},
 	},
 	{
-		"GET", "/path-params/:era/:group/:member",
-		"GET", "/path-params/60s/beatles/lennon", true,
+		"GET",
+		"/path-params/:era/:group/:member",
+		"GET",
+		"/path-params/60s/beatles/lennon",
+		true,
 		map[string]string{
 			"era":    "60s",
 			"group":  "beatles",
@@ -130,8 +229,11 @@ var tests = []struct {
 		},
 	},
 	{
-		"GET", "/path-params-prefix/:era/:group/:member/",
-		"GET", "/path-params-prefix/60s/beatles/lennon/yoko", true,
+		"GET",
+		"/path-params-prefix/:era/:group/:member/",
+		"GET",
+		"/path-params-prefix/60s/beatles/lennon/yoko",
+		true,
 		map[string]string{
 			"era":    "60s",
 			"group":  "beatles",
@@ -140,46 +242,70 @@ var tests = []struct {
 	},
 	// misc no matches
 	{
-		"GET", "/not/enough",
-		"GET", "/not/enough/items", false, nil,
+		"GET",
+		"/not/enough",
+		"GET",
+		"/not/enough/items",
+		false,
+		nil,
 	},
 	{
-		"GET", "/not/enough/items",
-		"GET", "/not/enough", false, nil,
+		"GET",
+		"/not/enough/items",
+		"GET",
+		"/not/enough",
+		false,
+		nil,
 	},
 }
 
 func TestWay(t *testing.T) {
-	for _, test := range tests {
-		r := NewRouter()
-		match := false
-		var ctx context.Context
-		r.Handle(test.RouteMethod, test.RoutePattern, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			match = true
-			ctx = r.Context()
-		}))
-		req, err := http.NewRequest(test.Method, test.Path, nil)
-		if err != nil {
-			t.Errorf("NewRequest: %s", err)
-		}
-		w := httptest.NewRecorder()
-		r.ServeHTTP(w, req)
-		if match != test.Match {
-			t.Errorf("expected match %v but was %v: %s %s", test.Match, match, test.Method, test.Path)
-		}
-		if len(test.Params) > 0 {
-			for expK, expV := range test.Params {
-				// check using helper
-				actualValStr := Param(ctx, expK)
-				if actualValStr != expV {
-					t.Errorf("Param: context value %s expected \"%s\" but was \"%s\"", expK, expV, actualValStr)
+	t.Parallel()
+
+	for _, tt := range tests {
+		tt := tt
+		name := fmt.Sprintf("%s-%s-%s-%s-%v",
+			tt.RouteMethod,
+			tt.RoutePattern,
+			tt.Method,
+			tt.Path,
+			tt.Match,
+		)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			r := NewRouter()
+			match := false
+			var ctx context.Context
+			r.Handle(tt.RouteMethod, tt.RoutePattern, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				match = true
+				ctx = r.Context()
+			}))
+			req, err := http.NewRequest(tt.Method, tt.Path, nil)
+			if err != nil {
+				t.Errorf("NewRequest: %s", err)
+			}
+			w := httptest.NewRecorder()
+			r.ServeHTTP(w, req)
+			if match != tt.Match {
+				t.Errorf("expected match %v but was %v: %s %s", tt.Match, match, tt.Method, tt.Path)
+			}
+			if len(tt.Params) > 0 {
+				for expK, expV := range tt.Params {
+					// check using helper
+					actualValStr := Param(ctx, expK)
+					if actualValStr != expV {
+						t.Errorf("Param: context value %s expected \"%s\" but was \"%s\"", expK, expV, actualValStr)
+					}
 				}
 			}
-		}
+		})
 	}
 }
 
 func TestMultipleRoutesDifferentMethods(t *testing.T) {
+	t.Parallel()
+
 	r := NewRouter()
 	var match string
 	r.Handle(http.MethodGet, "/route", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
