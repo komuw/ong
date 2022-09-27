1a2f36106f17ee1fd9f94a6b9d89a435c5db8d76
$MY_NAME <komuw05@gmail.com>
2022-09-27 16:27:14 +0300

    f

--- server/server.go
+++ server/server.go
@@ -112,7 +112,11 @@ func NewOpts(
 // DevOpts returns a new opts that has sensible defaults for tls, especially for dev environments.
 // It also automatically creates the dev certifiates/key by internally calling [CreateDevCertKey]
 func DevOpts() opts {
-	_, _ = CreateDevCertKey()
+	if os.Getenv("ONG_RUNNING_IN_TESTS") != "" {
+		// This function call fails in CI with `permission denied`
+		// since it is trying to create certificates in filesystem.
+		_, _ = CreateDevCertKey()
+	}
 	certFile, keyFile := certKeyPaths()
 	return withOpts(65081, certFile, keyFile, "", "localhost")
 }
