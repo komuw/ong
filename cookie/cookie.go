55ee32305ca2c254104655281e2e330f99b101ca
$MY_NAME <komuw05@gmail.com>
2022-10-04 12:34:02 +0300

    f

--- cookie/cookie.go
+++ cookie/cookie.go
@@ -95,9 +95,8 @@ var (
 // The cookie value(but not the name) is encrypted and authenticated using [cry.Enc].
 //
 // Note: While encrypted cookies can guarantee that the data has not been tampered with,
-// that it is all there and correct, and that the clients cannot read its raw value.
-// They cannot guarantee freshness i.e. that you are being sent back the last thing you sent to the client.
-// This means that (similar to plain-text cookies), it is still susceptible to [replay attacks]
+// that it is all there and correct, and that the clients cannot read its raw value; they cannot guarantee freshness.
+// This means that (similar to plain-text cookies), they are still susceptible to [replay attacks]
 //
 // Also see [Set]
 //
