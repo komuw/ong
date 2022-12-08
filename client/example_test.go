57c70d2d77ca26ccd1872c88942500fb50883a2b
$MY_NAME <komuw05@gmail.com>
2022-12-08 11:54:10 +0300

    f

--- client/example_test.go
+++ client/example_test.go
@@ -1,7 +1,6 @@
 package client_test
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/komuw/ong/client"
@@ -14,8 +13,7 @@ func ExampleSafe() {
 	cli := client.Safe(l)
 	// This is the AWS metadata url.
 	url := "http://169.254.169.254/latest/meta-data"
-	res, err := cli.Get(url)
-	fmt.Println("res, err: ", res, err)
+	_, _ = cli.Get(url)
 
 	// This will log something like:
 	// {"durationMS":0,"err":"dial tcp 169.254.169.254:80: ong/client: address 169.254.169.254 IsLinkLocalUnicast","level":"error","logID":"yHSDRXAJP7QKx3AJQKNt7w","method":"GET","msg":"http_client","pid":325431,"timestamp":"2022-12-08T08:43:42.151691969Z","url":"http://169.254.169.254/latest/meta-data"}
