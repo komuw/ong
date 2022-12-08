f0c39c81820c3e79f7b58086a46e279c784fc648
$MY_NAME <komuw05@gmail.com>
2022-12-08 11:42:07 +0300

    f

--- client/client.go
+++ client/client.go
@@ -3,15 +3,11 @@
 package client
 
 import (
-	"context"
 	"fmt"
-	"io"
 	"net"
 	"net/http"
 	"net/netip"
-	"net/url"
 	"os"
-	"strings"
 	"syscall"
 	"time"
 
@@ -26,30 +22,52 @@ const logIDHeader = string(log.CtxKey)
 // as of 9th/september/2022
 //
 
-// Safe creates a client that is safe from the server-side request forgery (SSRF) security vulnerability.
-func Safe(l log.Logger) *Client {
+// Safe creates a http client that has some good defaults & is safe from server-side request forgery (SSRF).
+// It also logs requests and responses using [log.Logger]
+//
+// Clients should be reused instead of created as needed. Clients are safe for concurrent use by multiple goroutines.
+func Safe(l log.Logger) *http.Client {
 	return new(true, l)
 }
 
-// Unsafe creates a client that is NOT safe from the server-side request forgery (SSRF) security vulnerability.
-func Unsafe(l log.Logger) *Client {
+// Unsafe creates a http client that has some good defaults & is NOT safe from server-side request forgery (SSRF).
+// It also logs requests and responses using [log.Logger]
+//
+// Clients should be reused instead of created as needed. Clients are safe for concurrent use by multiple goroutines.
+func Unsafe(l log.Logger) *http.Client {
 	return new(false, l)
 }
 
-// Client is a [http.Client] that has some good defaults. It also logs requests and responses using [log.Logger]
-//
-// Use either [Safe] or [Unsafe] to get a valid client.
-//
-// Clients should be reused instead of created as needed. Clients are safe for concurrent use by multiple goroutines.
-//
-// see [http.Client]
-type Client struct {
-	cli *http.Client
-	l   log.Logger
+// loggingRT is a [http.RoundTripper] that logs requests and responses.
+type loggingRT struct {
+	l log.Logger
+	http.RoundTripper
+}
+
+func (lr *loggingRT) RoundTrip(req *http.Request) (res *http.Response, err error) {
+	start := time.Now()
+	defer func() {
+		l := lr.l.WithCtx(req.Context())
+		flds := log.F{
+			"msg":        "http_client",
+			"method":     req.Method,
+			"url":        req.URL.Redacted(),
+			"durationMS": time.Since(start).Milliseconds(),
+		}
+		if err != nil {
+			l.Error(err, flds)
+		} else {
+			flds["code"] = res.StatusCode
+			flds["status"] = res.Status
+			l.Info(flds)
+		}
+	}()
+
+	return lr.RoundTripper.RoundTrip(req)
 }
 
 // new creates a client. Use [Safe] or [Unsafe] instead.
-func new(ssrfSafe bool, l log.Logger) *Client {
+func new(ssrfSafe bool, l log.Logger) *http.Client {
 	// The wikipedia monitoring dashboards are public: https://grafana.wikimedia.org/?orgId=1
 	// In there we can see that the p95 response times for http GET requests is ~700ms: https://grafana.wikimedia.org/d/RIA1lzDZk/application-servers-red?orgId=1
 	// and the p95 response times for http POST requests is ~3seconds:
@@ -79,116 +97,15 @@ func new(ssrfSafe bool, l log.Logger) *Client {
 		TLSHandshakeTimeout:   timeout,
 		ExpectContinueTimeout: 1 * time.Second,
 	}
-	cli := &http.Client{
-		Transport: transport,
+
+	lr := &loggingRT{
+		RoundTripper: transport,
+		l:            l.WithFields(log.F{"pid": os.Getpid()}),
+	}
+
+	return &http.Client{
+		Transport: lr,
 		Timeout:   timeout,
-	}
-
-	return &Client{
-		cli: cli,
-		l:   l.WithFields(log.F{"pid": os.Getpid()}),
-	}
-}
-
-// CloseIdleConnections closes any connections on its Transport which were previously connected from previous requests but are now sitting idle in a "keep-alive" state.
-//
-// It does not interrupt any connections currently in use.
-//
-// see [http.Client.CloseIdleConnections]
-func (c *Client) CloseIdleConnections() {
-	c.cli.CloseIdleConnections()
-}
-
-// Do sends an HTTP request and returns an HTTP response, following policy (such as redirects, cookies, auth) as configured on the client.
-//
-// see [http.Client.Do]
-func (c *Client) Do(ctx context.Context, req *http.Request) (resp *http.Response, err error) {
-	end := c.log(ctx, req.URL.EscapedPath(), req.Method)
-	defer func() {
-		end(resp, err)
-	}()
-
-	req = req.WithContext(ctx)
-	req.Header.Set(logIDHeader, log.GetId(ctx))
-	return c.cli.Do(req)
-}
-
-// Get issues a GET to the specified URL.
-//
-// see [http.Client.Get]
-func (c *Client) Get(ctx context.Context, url string) (resp *http.Response, err error) {
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	return c.Do(ctx, req)
-}
-
-// Head issues a HEAD to the specified URL.
-//
-// see [http.Client.Head]
-func (c *Client) Head(ctx context.Context, url string) (resp *http.Response, err error) {
-	req, err := http.NewRequest(http.MethodHead, url, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	return c.Do(ctx, req)
-}
-
-// Post issues a POST to the specified URL.
-//
-// see [http.Client.Post]
-func (c *Client) Post(ctx context.Context, url, contentType string, body io.Reader) (resp *http.Response, err error) {
-	req, err := http.NewRequest(http.MethodPost, url, body)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Content-Type", contentType)
-
-	return c.Do(ctx, req)
-}
-
-// PostForm issues a POST to the specified URL, with data's keys and values URL-encoded as the request body.
-//
-// see [http.Client.PostForm]
-func (c *Client) PostForm(ctx context.Context, url string, data url.Values) (resp *http.Response, err error) {
-	return c.Post(ctx, url, "application/x-www-form-urlencoded", strings.NewReader(data.Encode()))
-}
-
-func (c *Client) log(ctx context.Context, url, method string) func(resp *http.Response, err error) {
-	l := c.l.WithCtx(ctx)
-	l.Info(log.F{
-		"msg":     "http_client",
-		"process": "request",
-		"method":  method,
-		"url":     url,
-	})
-
-	start := time.Now()
-
-	return func(resp *http.Response, err error) {
-		flds := log.F{
-			"msg":        "http_client",
-			"process":    "response",
-			"method":     "GET",
-			"url":        url,
-			"durationMS": time.Since(start).Milliseconds(),
-		}
-		if resp != nil {
-			flds["code"] = resp.StatusCode
-			flds["status"] = resp.Status
-
-			if resp.StatusCode >= http.StatusBadRequest {
-				// both client and server errors.
-				l.Error(err, flds)
-			} else {
-				l.Info(flds)
-			}
-		} else {
-			l.Error(err, flds)
-		}
 	}
 }
 
--- client/example_test.go
+++ client/example_test.go
@@ -1,20 +1,21 @@
 package client_test
 
 import (
-	"context"
+	"fmt"
 	"os"
 
 	"github.com/komuw/ong/client"
 	"github.com/komuw/ong/log"
 )
 
-func ExampleClient_Get() {
+func ExampleSafe() {
 	l := log.New(os.Stdout, 7)
 
 	cli := client.Safe(l)
 	// This is the AWS metadata url.
 	url := "http://169.254.169.254/latest/meta-data"
-	_, _ = cli.Get(context.Background(), url)
+	res, err := cli.Get(url)
+	fmt.Println("res, err: ", res, err)
 
 	// This will log something like:
 	// {"level":"info","logID":"Z5X7qXm8HkT8kZ83xQyrrQ","method":"GET","msg":"http_client","pid":11776,"process":"request","timestamp":"2022-10-09T12:03:33.851543383Z","url":"http://169.254.169.254/latest/meta-data"}
