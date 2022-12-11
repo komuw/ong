a7a8e3a79f82cf74651822130f0852ed32f51b03
$MY_NAME <komuw05@gmail.com>
2022-12-11 17:47:52 +0300

    f

--- middleware/client_ip.go
+++ middleware/client_ip.go
@@ -78,7 +78,7 @@ func SingleIpStrategy(headerName string) clientIPstrategy {
 	return clientIPstrategy(headerName)
 }
 
-// ClientIP fetches the "real" client IP address from r.
+// ClientIP returns the "real" client IP address.
 //
 // Warning: This should be used with care. Clients CAN easily spoof IP addresses.
 // Fetching the "real" client is done in a best-effort basis and can be [grossly inaccurate & precarious].
