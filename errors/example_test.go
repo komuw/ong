f65f144a813cbaae9d457049e6aee912f174d7f0
$MY_NAME <komuw05@gmail.com>
2023-01-16 22:52:47 +0300

    f

--- errors/example_test.go
+++ errors/example_test.go
@@ -2,8 +2,10 @@ package errors_test
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/komuw/ong/errors"
+	"golang.org/x/exp/slices"
 )
 
 const expectedUser = "admin"
@@ -19,4 +21,27 @@ func login(user string) error {
 func Example_stackTraceFormatting() {
 	err := login("badGuy")
 	fmt.Printf("%+v", err)
+}
+
+func ExampleWrap() {
+	_, err := os.Open("/this/file/does/not/exist.txt")
+	if err != nil {
+		errors.Wrap(err)
+	}
+}
+
+func ExampleDwrap() {
+	fetchUser := func(u string) (errp error) {
+		defer errors.Dwrap(&errp)
+
+		users := []string{"John", "Alice", "Kamau"}
+		if !slices.Contains(users, u) {
+			return fmt.Errorf("user %s not found", u)
+		}
+
+		return nil
+	}
+
+	e := fetchUser("Emmy")
+	fmt.Printf("%+#v", e)
 }
