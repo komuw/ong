167d39d4ea72028e799ba521ad09e342bdfc3723
Komu Wairagu <komuw05@gmail.com>
2023-01-17 12:43:55 +0300

    Improve examples (#204)

---
+++ example/handlers.go
@@ -0,0 +1,280 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/komuw/ong/client"
+	"github.com/komuw/ong/cookie"
+	"github.com/komuw/ong/cry"
+	"github.com/komuw/ong/errors"
+	"github.com/komuw/ong/id"
+	"github.com/komuw/ong/log"
+	"github.com/komuw/ong/middleware"
+	"github.com/komuw/ong/mux"
+	"github.com/komuw/ong/sess"
+	"github.com/komuw/ong/xcontext"
+)
+
+// db is a dummy database.
+type db interface {
+	Get(key string) string
+	Set(key, value string)
+}
+
+// app represents component as a struct, shared dependencies as fields, no global state.
+type app struct {
+	db db
+	l  log.Logger
+}
+
+// NewApp creates a new app.
+func NewApp(d db) app {
+	return app{
+		db: d,
+		l:  log.New(os.Stdout, 1000),
+	}
+}
+
+// health handler showcases the use of:
+// - encryption/decryption.
+// - random id.
+func (a app) health(secretKey string) http.HandlerFunc {
+	var (
+		once        sync.Once
+		serverBoot  time.Time = time.Now().UTC()
+		serverStart time.Time
+		serverID    = id.New()
+		enc         = cry.New(secretKey)
+	)
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		// intialize somethings only once for perfomance.
+		once.Do(func() {
+			serverStart = time.Now().UTC()
+		})
+
+		encryptedSrvID := enc.EncryptEncode(serverID)
+
+		res := fmt.Sprintf("serverBoot=%s, serverStart=%s, serverId=%s\n", serverBoot, serverStart, encryptedSrvID)
+		_, _ = io.WriteString(w, res)
+	}
+}
+
+// check handler showcases the use of:
+// - mux.Param.
+// - sessions.
+// - xcontext.Detach.
+// - safe http client.
+// - error wrapping.
+func (a app) check(msg string) http.HandlerFunc {
+	cli := client.Safe(a.l)
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		cartID := "afakHda8eqL"
+
+		age := mux.Param(r.Context(), "age")
+		sess.SetM(r, sess.M{
+			"name":    "John Doe",
+			"age":     age,
+			"cart_id": cartID,
+		})
+
+		if sess.Get(r, "cart_id") != "" {
+			if sess.Get(r, "cart_id") != cartID {
+				http.Error(w, "wrong cartID", http.StatusBadRequest)
+				return
+			}
+		}
+
+		go func(ctx context.Context) {
+			makeReq := func(url string) (code int, errp error) {
+				defer errors.Dwrap(&errp)
+
+				req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+				if err != nil {
+					return 0, err
+				}
+				resp, err := cli.Do(req)
+				if err != nil {
+					return 0, err
+				}
+				defer func() { _ = resp.Body.Close() }()
+
+				return resp.StatusCode, nil
+			}
+
+			l := a.l.WithCtx(ctx)
+			code, err := makeReq("https://example.com")
+			if err != nil {
+				l.Error(err)
+			}
+			l.Info(log.F{"msg": "req succeded", "code": code})
+		}(
+			// we need to detach context,
+			// since this goroutine can outlive the http request lifecycle.
+			xcontext.Detach(r.Context()),
+		)
+
+		_, _ = fmt.Fprintf(w, "hello %s. Age is %s", msg, age)
+	}
+}
+
+// login handler showcases the use of:
+// - csrf tokens.
+// - csp tokens.
+// - encrypted cookies
+// - hashing passwords.
+func (a app) login(secretKey string) http.HandlerFunc {
+	tmpl, err := template.New("myTpl").Parse(`<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://unpkg.com/mvp.css@1.12/mvp.css">
+<style>
+	:root{
+		/* ovverive variables from mvp.css */
+		--line-height: 1.0;
+		--font-family: system-ui;
+	}
+	html {
+	/*
+	from:
+	  - https://www.swyx.io/css-100-bytes
+	  - https://news.ycombinator.com/item?id=32972768
+	  - https://github.com/andybrewer/mvp
+	  - https://www.joshwcomeau.com/css/full-bleed/
+	  - https://www.joshwcomeau.com/css/custom-css-reset/
+	*/
+		max-width: 70ch;
+		padding: 3em 1em;
+		margin: auto;
+		font-size: 1.0em;
+	}
+</style>
+</head>
+<body>
+    <script nonce="{{.CspNonceValue}}">
+	    console.log("hello world");
+	</script>
+
+	<h2>Welcome to awesome website.</h2>
+	<form method="POST">
+	<label>First Name:</label><br>
+	<input type="text" id="firstName" name="firstName"><br>
+	<label>Email:</label><br>
+	<input type="text" id="email" name="email"><br>
+	<label>Password:</label><br>
+	<input type="password" id="password" name="password"><br>
+	
+	<input type="hidden" id="{{.CsrfTokenName}}" name="{{.CsrfTokenName}}" value="{{.CsrfTokenValue}}"><br>
+	<input type="submit">
+	</form>
+
+</body>
+</html>`)
+	if err != nil {
+		panic(err)
+	}
+
+	type User struct {
+		Email string
+		Name  string
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		reqL := a.l.WithCtx(r.Context())
+
+		if r.Method != http.MethodPost {
+			data := struct {
+				CsrfTokenName  string
+				CsrfTokenValue string
+				CspNonceValue  string
+			}{
+				CsrfTokenName:  middleware.CsrfTokenFormName,
+				CsrfTokenValue: middleware.GetCsrfToken(r.Context()),
+				CspNonceValue:  middleware.GetCspNonce(r.Context()),
+			}
+
+			if err = tmpl.Execute(w, data); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			return
+		}
+
+		if err = r.ParseForm(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		email := r.FormValue("email")
+		firstName := r.FormValue("firstName")
+		password := r.FormValue("password")
+
+		u := &User{Email: email, Name: firstName}
+
+		s, errM := json.Marshal(u)
+		if errM != nil {
+			http.Error(w, errM.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		cookieName := "example_session_cookie"
+		c, errM := cookie.GetEncrypted(r, cookieName, secretKey)
+		reqL.WithImmediate().Info(log.F{
+			"msg":    "login handler log cookie",
+			"err":    errM,
+			"cookie": c,
+		})
+
+		cookie.SetEncrypted(
+			r,
+			w,
+			cookieName,
+			string(s),
+			"localhost",
+			23*24*time.Hour,
+			secretKey,
+		)
+
+		existingPasswdHash := a.db.Get("passwd")
+		if e := cry.Eql(password, existingPasswdHash); e != nil {
+			// passwd did not exist before.
+			hashedPasswd, errH := cry.Hash(password)
+			if errH != nil {
+				http.Error(w, errH.Error(), http.StatusInternalServerError)
+				return
+			}
+			a.db.Set("passwd", hashedPasswd)
+		}
+
+		_, _ = fmt.Fprintf(w, "you have submitted: %s", r.Form)
+	}
+}
+
+// handleFileServer handler showcases the use of:
+// - middleware.ClientIP
+// - logging
+func (a app) handleFileServer() http.HandlerFunc {
+	// Do NOT let `http.FileServer` be able to serve your root directory.
+	// Otherwise, your .git folder and other sensitive info(including http://localhost:65080/main.go) may be available
+	// instead create a folder that only has your templates and server that.
+	fs := http.FileServer(http.Dir("./stuff"))
+	realHandler := http.StripPrefix("somePrefix", fs).ServeHTTP
+	return func(w http.ResponseWriter, r *http.Request) {
+		reqL := a.l.WithCtx(r.Context())
+
+		reqL.Info(log.F{"msg": "handleFileServer", "clientIP": middleware.ClientIP(r)})
+
+		reqL.StdLogger().Println("this is now a Go standard library logger")
+
+		realHandler(w, r)
+	}
+}
--- example/main.go
+++ example/main.go
@@ -1,40 +1,26 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"html/template"
-	"io"
-	"net/http"
 	"os"
-	"sync"
-	"time"
 
-	"github.com/komuw/ong/cookie"
 	"github.com/komuw/ong/log"
 	"github.com/komuw/ong/middleware"
 	"github.com/komuw/ong/mux"
 	"github.com/komuw/ong/server"
-	"github.com/komuw/ong/sess"
 )
 
-// Taken mainly from the talk; "How I Write HTTP Web Services after Eight Years" by Mat Ryer
-// 1. https://www.youtube.com/watch?v=rWBSMsLG8po
-// 2. https://pace.dev/blog/2018/05/09/how-I-write-http-services-after-eight-years.html
-
-const secretKey = "hard-password"
-
 func main() {
-	api := NewMyApi("someDb")
+	const secretKey = "super-h@rd-pa$$word"
+	api := NewApp(myDB{map[string]string{}})
 	l := log.New(os.Stdout, 1000)
 	mux := mux.New(
 		l,
 		middleware.WithOpts("localhost", 65081, secretKey, middleware.DirectIpStrategy, l),
 		nil,
 		mux.NewRoute(
-			"/api",
+			"/health",
 			mux.MethodPost,
-			api.handleAPI(),
+			api.health(secretKey),
 		),
 		mux.NewRoute(
 			"serveDirectory",
@@ -49,7 +35,7 @@ func main() {
 		mux.NewRoute(
 			"login",
 			mux.MethodAll,
-			api.login(),
+			api.login(secretKey),
 		),
 	)
 
@@ -60,202 +46,8 @@ func main() {
 	}
 }
 
-// myAPI represents component as a struct, shared dependencies as fields, no global state
-type myAPI struct {
-	db string
-	l  log.Logger
-}
+// myDB implements a dummy in-memory database.
+type myDB struct{ m map[string]string }
 
-func NewMyApi(db string) myAPI {
-	return myAPI{
-		db: db,
-		l:  log.New(os.Stdout, 1000),
-	}
-}
-
-func (m myAPI) handleFileServer() http.HandlerFunc {
-	// Do NOT let `http.FileServer` be able to serve your root directory.
-	// Otherwise, your .git folder and other sensitive info(including http://localhost:65080/main.go) may be available
-	// instead create a folder that only has your templates and server that.
-	fs := http.FileServer(http.Dir("./stuff"))
-	realHandler := http.StripPrefix("somePrefix", fs).ServeHTTP
-	return func(w http.ResponseWriter, r *http.Request) {
-		reqL := m.l.WithCtx(r.Context())
-
-		reqL.Info(log.F{"msg": "handleFileServer", "redactedURL": r.URL.Redacted()})
-		realHandler(w, r)
-	}
-}
-
-// Handlers are methods on the server which gives them access to dependencies
-// Remember, other handlers have access to `s` too, so be careful with data races
-// Why return `http.HandlerFunc` instead of `http.Handler`?
-// `HandlerFunc` implements `Handler` interface so they are kind of interchangeable
-// Pick whichever is easier for you to use. Sometimes you might have to convert between them
-func (m myAPI) handleAPI() http.HandlerFunc {
-	// allows for handler specific setup
-	thing := func() int {
-		return 42
-	}
-	var once sync.Once
-	var serverStart time.Time
-
-	return func(w http.ResponseWriter, r *http.Request) {
-		reqL := m.l.WithCtx(r.Context())
-
-		// intialize somethings only once for perf
-		once.Do(func() {
-			reqL.Info(log.F{"msg": "called only once during the first request"})
-			serverStart = time.Now()
-		})
-
-		// use thing
-		ting := thing()
-		if ting != 42 {
-			http.Error(w, "thing ought to be 42", http.StatusBadRequest)
-			return
-		}
-
-		res := fmt.Sprintf("serverStart=%v\n. Hello. answer to life is %v \n", serverStart, ting)
-		_, _ = io.WriteString(w, res)
-	}
-}
-
-// you can take arguments for handler specific dependencies
-func (m myAPI) check(msg string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		reqL := m.l.WithCtx(r.Context())
-
-		cspNonce := middleware.GetCspNonce(r.Context())
-		csrfToken := middleware.GetCsrfToken(r.Context())
-		reqL.Info(log.F{"msg": "check called", "cspNonce": cspNonce, "csrfToken": csrfToken})
-
-		cartID := "afakHda8eqL"
-		sess.SetM(r, sess.M{
-			"name":    "John Doe",
-			"age":     "88",
-			"cart_id": cartID,
-		})
-
-		reqL.WithImmediate().Info(log.F{"cart_id": sess.Get(r, "cart_id")})
-		if sess.Get(r, "cart_id") != "" {
-			if sess.Get(r, "cart_id") != cartID {
-				panic("wrong cartID")
-			}
-		}
-
-		age := mux.Param(r.Context(), "age")
-		// use msg, which is a dependency specific to this handler
-		_, _ = fmt.Fprintf(w, "hello %s. Age is %s", msg, age)
-	}
-}
-
-func (m myAPI) login() http.HandlerFunc {
-	tmpl, err := template.New("myTpl").Parse(`<!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" href="https://unpkg.com/mvp.css@1.12/mvp.css">
-<style>
-	:root{
-		/* ovverive variables from mvp.css */
-		--line-height: 1.0;
-		--font-family: system-ui;
-	}
-	html {
-	/*
-	from:
-	  - https://www.swyx.io/css-100-bytes
-	  - https://news.ycombinator.com/item?id=32972768
-	  - https://github.com/andybrewer/mvp
-	  - https://www.joshwcomeau.com/css/full-bleed/
-	  - https://www.joshwcomeau.com/css/custom-css-reset/
-	*/
-		max-width: 70ch;
-		padding: 3em 1em;
-		margin: auto;
-		font-size: 1.0em;
-	}
-</style>
-</head>
-<body>
-    <script nonce="{{.CspNonceValue}}">
-	    console.log("hello world");
-	</script>
-
-	<h2>Welcome to awesome website.</h2>
-	<form method="POST">
-	<label>Email:</label><br>
-	<input type="text" id="email" name="email"><br>
-	<label>First Name:</label><br>
-	<input type="text" id="firstName" name="firstName"><br>
-
-	<input type="hidden" id="{{.CsrfTokenName}}" name="{{.CsrfTokenName}}" value="{{.CsrfTokenValue}}"><br>
-	<input type="submit">
-	</form>
-
-</body>
-</html>`)
-	if err != nil {
-		panic(err)
-	}
-
-	type User struct {
-		Email string
-		Name  string
-	}
-
-	return func(w http.ResponseWriter, r *http.Request) {
-		reqL := m.l.WithCtx(r.Context())
-
-		if r.Method != http.MethodPost {
-			data := struct {
-				CsrfTokenName  string
-				CsrfTokenValue string
-				CspNonceValue  string
-			}{
-				CsrfTokenName:  middleware.CsrfTokenFormName,
-				CsrfTokenValue: middleware.GetCsrfToken(r.Context()),
-				CspNonceValue:  middleware.GetCspNonce(r.Context()),
-			}
-
-			if err = tmpl.Execute(w, data); err != nil {
-				panic(err)
-			}
-			return
-		}
-
-		if err = r.ParseForm(); err != nil {
-			panic(err)
-		}
-
-		email := r.FormValue("email")
-		firstName := r.FormValue("firstName")
-
-		u := &User{Email: email, Name: firstName}
-
-		s, errM := json.Marshal(u)
-		if errM != nil {
-			panic(errM)
-		}
-
-		cookieName := "ong_example_session_cookie"
-		c, errM := cookie.GetEncrypted(r, cookieName, secretKey)
-		reqL.WithImmediate().Info(log.F{
-			"msg":    "login handler log cookie",
-			"err":    errM,
-			"cookie": c,
-		})
-
-		cookie.SetEncrypted(
-			r,
-			w,
-			cookieName,
-			string(s),
-			"localhost",
-			23*24*time.Hour,
-			secretKey,
-		)
-
-		_, _ = fmt.Fprintf(w, "you have submitted: %s", r.Form)
-	}
-}
+func (m myDB) Get(key string) string { return "" }
+func (m myDB) Set(key, value string) {}
