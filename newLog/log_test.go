508f233b8b5137339a5769a1ccf7d6840437f143
$MY_NAME <komuw05@gmail.com>
2023-01-29 00:39:14 +0300

    m

--- newLog/log_test.go
+++ newLog/log_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
 	"sync"
 	"testing"
 
@@ -192,27 +193,27 @@ func TestLogger(t *testing.T) {
 		}
 	})
 
-	// t.Run("logs are rotated", func(t *testing.T) {
-	// 	t.Parallel()
+	t.Run("logs are rotated", func(t *testing.T) {
+		t.Parallel()
 
-	// 	w := &bytes.Buffer{}
-	// 	maxMsgs := 3
-	// 	l := New(w, maxMsgs)
+		w := &bytes.Buffer{}
+		maxMsgs := 3
+		l := NewSlog(w, maxMsgs)
 
-	// 	for i := 0; i <= (maxMsgs + 4); i++ {
-	// 		infoMsg := "hello world" + " : " + fmt.Sprint(i)
-	// 		l.Info(F{"what": infoMsg})
-	// 	}
-	// 	errMsg := "oops, Houston we got 99 problems."
-	// 	l.Error(errors.New(errMsg))
+		for i := 0; i <= (maxMsgs + 4); i++ {
+			infoMsg := "hello world" + " : " + fmt.Sprint(i)
+			l(context.Background()).Info(infoMsg)
+		}
+		errMsg := "oops, Houston we got 99 problems."
+		l(context.Background()).Error("somer-error", errors.New(errMsg))
 
-	// 	attest.False(t, strings.Contains(w.String(), "hello world : 1"))
-	// 	attest.False(t, strings.Contains(w.String(), "hello world : 2"))
-	// 	attest.False(t, strings.Contains(w.String(), "hello world : 5"))
-	// 	attest.Subsequence(t, w.String(), "hello world : 6")
-	// 	attest.Subsequence(t, w.String(), "hello world : 7")
-	// 	attest.Subsequence(t, w.String(), errMsg)
-	// })
+		attest.False(t, strings.Contains(w.String(), "hello world : 1"))
+		attest.False(t, strings.Contains(w.String(), "hello world : 2"))
+		attest.False(t, strings.Contains(w.String(), "hello world : 5"))
+		attest.Subsequence(t, w.String(), "hello world : 6")
+		attest.Subsequence(t, w.String(), "hello world : 7")
+		attest.Subsequence(t, w.String(), errMsg)
+	})
 
 	// t.Run("various ways of calling l.Error", func(t *testing.T) {
 	// 	t.Parallel()
