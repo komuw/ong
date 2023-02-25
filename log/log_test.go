73c116298a1cc395fec003132413a1bf9068587e
$MY_NAME <komuw05@gmail.com>
2023-02-25 10:59:18 +0300

    m

--- log/log_test.go
+++ log/log_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/akshayjshah/attest"
@@ -177,27 +178,27 @@ func TestLogger(t *testing.T) {
 		}
 	})
 
-	// t.Run("logs are rotated", func(t *testing.T) {
-	// 	t.Parallel()
+	t.Run("logs are rotated", func(t *testing.T) {
+		t.Parallel()
 
-	// 	w := &bytes.Buffer{}
-	// 	maxMsgs := 3
-	// 	l := New(w, maxMsgs)
+		w := &bytes.Buffer{}
+		maxMsgs := 3
+		l := New(w, maxMsgs)
 
-	// 	for i := 0; i <= (maxMsgs + 4); i++ {
-	// 		infoMsg := "hello world" + " : " + fmt.Sprint(i)
-	// 		l(context.Background()).Info(infoMsg)
-	// 	}
-	// 	errMsg := "oops, Houston we got 99 problems."
-	// 	l(context.Background()).Error("somer-error", errors.New(errMsg))
+		for i := 0; i <= (maxMsgs + 4); i++ {
+			infoMsg := "hello world" + " : " + fmt.Sprint(i)
+			l(context.Background()).Info(infoMsg)
+		}
+		errMsg := "oops, Houston we got 99 problems."
+		l(context.Background()).Error("somer-error", errors.New(errMsg))
 
-	// 	attest.False(t, strings.Contains(w.String(), "hello world : 1"))
-	// 	attest.False(t, strings.Contains(w.String(), "hello world : 2"))
-	// 	attest.False(t, strings.Contains(w.String(), "hello world : 5"))
-	// 	attest.Subsequence(t, w.String(), "hello world : 6")
-	// 	attest.Subsequence(t, w.String(), "hello world : 7")
-	// 	attest.Subsequence(t, w.String(), errMsg)
-	// })
+		attest.False(t, strings.Contains(w.String(), "hello world : 1"))
+		attest.False(t, strings.Contains(w.String(), "hello world : 2"))
+		attest.False(t, strings.Contains(w.String(), "hello world : 5"))
+		attest.Subsequence(t, w.String(), "hello world : 6")
+		attest.Subsequence(t, w.String(), "hello world : 7")
+		attest.Subsequence(t, w.String(), errMsg)
+	})
 
 	// t.Run("id reused across contexts", func(t *testing.T) {
 	// 	t.Parallel()
