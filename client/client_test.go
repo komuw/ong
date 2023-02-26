f9112b272fd3b513c42100fef1ea2624339ece05
$MY_NAME <komuw05@gmail.com>
2023-02-26 20:45:57 +0300

    m

--- client/client_test.go
+++ client/client_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/akshayjshah/attest"
 	"github.com/komuw/ong/log"
+	"go.uber.org/goleak"
 	"golang.org/x/exp/slog"
 )
 
@@ -18,11 +19,10 @@ func getLogger() *slog.Logger {
 	return log.New(w, maxMsgs)(context.Background())
 }
 
-// TODO: (komuw)
-// func TestMain(m *testing.M) {
-// 	// call flag.Parse() here if TestMain uses flags
-// 	goleak.VerifyTestMain(m)
-// }
+func TestMain(m *testing.M) {
+	// call flag.Parse() here if TestMain uses flags
+	goleak.VerifyTestMain(m)
+}
 
 func TestClient(t *testing.T) {
 	t.Parallel()
@@ -38,12 +38,14 @@ func TestClient(t *testing.T) {
 		"https://www.example.com",
 	}
 
-	clean := func(res *http.Response) {
-		t.Cleanup(func() {
-			if res != nil {
-				res.Body.Close()
-			}
-		})
+	clean := func(res *http.Response, cli *http.Client) {
+		if res != nil {
+			res.Body.Close()
+		}
+
+		// Without this, `uber/goleak` would report a leak.
+		// see: https://github.com/uber-go/goleak/issues/87
+		cli.CloseIdleConnections()
 	}
 
 	t.Run("ssrf safe", func(t *testing.T) {
@@ -53,16 +55,16 @@ func TestClient(t *testing.T) {
 
 		for _, url := range urlsInPrivate {
 			res, err := cli.Get(url) // nolint:bodyclose
-			clean(res)
 			attest.Error(t, err)
 			attest.True(t, strings.Contains(err.Error(), errPrefix))
+			clean(res, cli)
 		}
 
 		for _, url := range urlsInPublic {
 			res, err := cli.Get(url) // nolint:bodyclose
-			clean(res)
 			attest.Ok(t, err)
 			attest.Equal(t, res.StatusCode, http.StatusOK, attest.Sprintf("url=%s", url))
+			clean(res, cli)
 		}
 	})
 
@@ -79,16 +81,16 @@ func TestClient(t *testing.T) {
 				break
 			}
 			res, err := cli.Get(url) // nolint:bodyclose
-			clean(res)
 			attest.Error(t, err)
 			attest.False(t, strings.Contains(err.Error(), errPrefix))
+			clean(res, cli)
 		}
 
 		for _, url := range urlsInPublic {
 			res, err := cli.Get(url) // nolint:bodyclose
-			clean(res)
 			attest.Ok(t, err)
 			attest.Equal(t, res.StatusCode, http.StatusOK, attest.Sprintf("url=%s", url))
+			clean(res, cli)
 		}
 	})
 
@@ -104,11 +106,11 @@ func TestClient(t *testing.T) {
 			cli := Safe(l)
 
 			res, err := cli.Get("https://ajmsmsYnns.com") // nolint:bodyclose
-			clean(res)
 			attest.Zero(t, res)
 			attest.Error(t, err)
 			attest.NotZero(t, w.String())
 			attest.Subsequence(t, w.String(), "ERROR")
+			clean(res, cli)
 		}
 
 		{
@@ -120,11 +122,11 @@ func TestClient(t *testing.T) {
 			cli := Safe(l)
 
 			res, err := cli.Get("https://example.com") // nolint:bodyclose
-			clean(res)
 			attest.NotZero(t, res)
 			attest.Ok(t, err)
 			attest.Zero(t, w.String())
 			attest.Equal(t, res.StatusCode, http.StatusOK)
+			clean(res, cli)
 		}
 	})
 
@@ -141,11 +143,11 @@ func TestClient(t *testing.T) {
 
 			b := strings.NewReader(`{"key":"value"}`)
 			res, err := cli.Post("https://ajmsmsYnns.com", "application/json", b) // nolint:bodyclose
-			clean(res)
 			attest.Zero(t, res)
 			attest.Error(t, err)
 			attest.NotZero(t, w.String())
 			attest.Subsequence(t, w.String(), "ERROR")
+			clean(res, cli)
 		}
 
 		{
@@ -158,11 +160,11 @@ func TestClient(t *testing.T) {
 
 			b := strings.NewReader(`{"key":"value"}`)
 			res, err := cli.Post("https://example.com", "application/json", b) // nolint:bodyclose
-			clean(res)
 			attest.NotZero(t, res)
 			attest.Ok(t, err)
 			attest.Zero(t, w.String())
 			attest.Equal(t, res.StatusCode, http.StatusOK)
+			clean(res, cli)
 		}
 	})
 }
