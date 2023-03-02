462caa2352af8af85c9327b1baf4cd7f519dc018
$MY_NAME <komuw05@gmail.com>
2023-03-02 15:06:29 +0300

    m

--- .github/workflows/integration_test_coverage.sh
+++ .github/workflows/integration_test_coverage.sh
@@ -94,11 +94,20 @@ load_test(){
   echo ''
 
   number_of_success=$(cat vegeta_results.json | jq '.status_codes."200"')
-  if [[ "$number_of_success" -ne 1783 ]] ; then
+  if [[ $"$number_of_success" -gt 1780 ]]; then
     # Actually, we would expect 1800 successes(20 *90) since the sending rate is 90/secs
     # which is below the ratelimit of 100/sec.
     # But ratelimiting is imprecise; https://github.com/komuw/ong/issues/235
-    echo "expected 1783 successful requests"
+    echo "expected at least $expected successful requests"
+    exit 61;
+  fi
+
+  errors=$(cat vegeta_results.json | jq '.errors[0]')
+  string='My long string'
+  if [[ $errors == *"429 Too Many Requests"* ]]; then
+    echo "" # it's there 
+  else
+    echo "expected ratelimiting errors"
     exit 61;
   fi
 }
