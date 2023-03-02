fd310bb2fbf0a893639b72718cc21bc63e598b65
$MY_NAME <komuw05@gmail.com>
2023-03-02 15:49:09 +0300

    m

--- example/integration_test.go
+++ example/integration_test.go
@@ -14,15 +14,24 @@ func TestIntegration(t *testing.T) {
 	t.Run("https_redirection", func(t *testing.T) {
 		t.Parallel()
 
-		c := &http.Client{
-			CheckRedirect: func(req *http.Request, via []*http.Request) error {
-				return http.ErrUseLastResponse
-			},
+		{
+			c := &http.Client{
+				CheckRedirect: func(req *http.Request, via []*http.Request) error {
+					return http.ErrUseLastResponse
+				},
+			}
+			res, err := c.Get("http://127.0.0.1:65080/health")
+			attest.Ok(t, err)
+			defer res.Body.Close()
+			attest.Equal(t, res.StatusCode, http.StatusPermanentRedirect)
 		}
-		res, err := c.Get("http://127.0.0.1:65080/health")
-		attest.Ok(t, err)
-		defer res.Body.Close()
-		attest.Equal(t, res.StatusCode, http.StatusPermanentRedirect)
 
+		{
+			c := &http.Client{}
+			res, err := c.Get("https://localhost:65081/health")
+			attest.Ok(t, err)
+			defer res.Body.Close()
+			attest.Equal(t, res.StatusCode, http.StatusOK)
+		}
 	})
 }
