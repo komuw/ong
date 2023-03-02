a69ace33adf2caea947ad822ab473d192eaea5e9
$MY_NAME <komuw05@gmail.com>
2023-03-02 15:29:21 +0300

    m

---
+++ example/tests/integration_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	vegeta "github.com/tsenart/vegeta/v12/lib"
+)
+
+func main() {
+	rate := vegeta.Rate{Freq: 100, Per: time.Second}
+	duration := 4 * time.Second
+	targeter := vegeta.NewStaticTargeter(vegeta.Target{
+		Method: "GET",
+		URL:    "http://localhost:9100/",
+	})
+	attacker := vegeta.NewAttacker()
+
+	var metrics vegeta.Metrics
+	for res := range attacker.Attack(targeter, rate, duration, "Big Bang!") {
+		metrics.Add(res)
+	}
+	metrics.Close()
+
+	fmt.Printf("99th percentile: %s\n", metrics.Latencies.P99)
+}
