870661d0ac7d4a5c7af6007b85baf2833d0e7e7f
$MY_NAME <komuw05@gmail.com>
2023-03-06 14:48:10 +0300

    m

--- log/log_test.go
+++ log/log_test.go
@@ -307,7 +307,7 @@ func TestLogger(t *testing.T) {
 			stdLogger := slog.NewLogLogger(l.Handler(), LevelImmediate)
 			stdLogger.Println(msg)
 			attest.Subsequence(t, w.String(), msg)
-			attest.Subsequence(t, w.String(), "log/log_test.go:307")
+			attest.Subsequence(t, w.String(), "log/log_test.go:308")
 			attest.True(t, LevelImmediate < 0) // otherwise it will trigger `log.handler` to flush all logs, which we dont want.
 		}
 	})
