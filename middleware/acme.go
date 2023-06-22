4121f97f12f9167ba5327f92d3c1c19fdb40dde8
$MY_NAME <komuw05@gmail.com>
2023-06-22 22:15:48 +0300

    m

--- middleware/acme.go
+++ middleware/acme.go
@@ -11,7 +11,13 @@ import (
 //   (a) https://github.com/golang/crypto/blob/master/acme/autocert/autocert.go whose license(BSD 3-Clause) can be found here: https://github.com/golang/crypto/blob/05595931fe9d3f8894ab063e1981d28e9873e2cb/LICENSE
 //
 
-// TODO: add docs.
+// acme is a middleware that handles ACME [http-01] challenge requests while delegating other requests to wrappedHandler.
+//
+// ACME CA sends challenge requests to `/.well-known/acme-challenge/` uri.
+// Note that this `http-01` challenge does not allow [wildcard] certificates.
+//
+// [http-01]: https://letsencrypt.org/docs/challenge-types
+// [wildcard]: https://letsencrypt.org/docs/faq/#does-let-s-encrypt-issue-wildcard-certificates
 func acme(wrappedHandler http.Handler, domain, acmeEmail, acmeDirectoryUrl string) http.HandlerFunc {
 	// `dmn.CertManager` should be called with valid domain.
 	// `middleware.New` validates the domain, so that by the time we get here, domain is valid.
