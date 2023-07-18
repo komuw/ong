afcbc105f11e928d5a5741a755db6c13887de40f
$MY_NAME <komuw05@gmail.com>
2023-07-18 22:09:01 +0300

    f

--- internal/acme/acme.go
+++ internal/acme/acme.go
@@ -15,7 +15,6 @@ import (
 	"strings"
 	"sync"
 	"time"
-	"unicode"
 
 	"golang.org/x/exp/slog"
 	"golang.org/x/net/idna"
@@ -179,11 +178,11 @@ func Handler(wrappedHandler http.Handler) http.HandlerFunc {
 					return
 				}
 
-				if len(domain) < 2 || unicode.IsDigit(rune(domain[0])) { // It is a bare IP.
-					errC := errors.New("ong/acme: request.host for well-known/acme-challenge request should not be IP address")
+				if _, errC := netip.ParseAddr(domain); errC == nil {
+					e := errors.New("ong/acme: request.host for well-known/acme-challenge request should not be IP address")
 					http.Error(
 						w,
-						errC.Error(),
+						e.Error(),
 						http.StatusTeapot,
 					)
 					return
