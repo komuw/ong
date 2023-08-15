c43ffb2004b2db5f481fcf07708f18092ebe5f66
$MY_NAME <komuw05@gmail.com>
2023-08-15 13:58:15 +0300

    add attest option

--- example/integration_test.go
+++ example/integration_test.go
@@ -132,13 +132,15 @@ func TestIntegration(t *testing.T) {
 		fmt.Println("\n.")
 
 		expectedSuccesses := 1782
-		attest.Approximately(t,
+		attest.Approximately(
+			t,
 			// Actually, we would expect 1800 successes(20 *90) since the sending rate is 90/secs
 			// which is below the ratelimit of 100/sec.
 			// But ratelimiting is imprecise; https://github.com/komuw/ong/issues/235
 			metrics.StatusCodes[fmt.Sprintf("%d", http.StatusOK)],
 			expectedSuccesses,
 			3,
+			attest.Sprintf("metrics = %v", metrics),
 		)
 
 		attest.Subsequence(t,
