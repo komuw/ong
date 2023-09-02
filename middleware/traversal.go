c0bf1d977731feb89d1412524cbd4401a3f7ecb2
$MY_NAME <komuw05@gmail.com>
2023-09-02 12:13:34 +0300

    m

--- middleware/traversal.go
+++ middleware/traversal.go
@@ -11,7 +11,6 @@ func pathTraversal(wrappedHandler http.Handler) http.HandlerFunc {
 		- https://github.com/golang/go/issues/54385
 	*/
 	return func(w http.ResponseWriter, r *http.Request) {
-		// TODO: integration tests
 		r.URL = r.URL.JoinPath()
 
 		wrappedHandler.ServeHTTP(w, r)
