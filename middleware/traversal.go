a0af0673d06f2016e240837c242f117e30fc921d
$MY_NAME <komuw05@gmail.com>
2023-09-02 11:34:59 +0300

    m

--- middleware/traversal.go
+++ middleware/traversal.go
@@ -7,6 +7,10 @@ import (
 
 // traversal is a middleware that tries to prevent path traversal attacks.
 func traversal(wrappedHandler http.Handler, domain string) http.HandlerFunc {
+	/*
+		- https://github.com/komuw/ong/issues/381
+		- https://github.com/golang/go/issues/54385
+	*/
 	return func(w http.ResponseWriter, r *http.Request) {
 		// TODO: integration tests
 		r.URL = r.URL.JoinPath()
