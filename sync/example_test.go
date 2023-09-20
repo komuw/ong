04ffe5442677c7c07c2eca89b1364e98daeedc47
$MY_NAME <komuw05@gmail.com>
2023-09-20 20:12:39 +0300

    m

---
+++ sync/example_test.go
@@ -0,0 +1,56 @@
+package sync_test
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/komuw/ong/sync"
+)
+
+// JustErrors illustrates the use of a Group in place of a sync.WaitGroup to
+// simplify goroutine counting and error handling.
+// This example is derived from the sync.WaitGroup example at https://golang.org/pkg/sync/#example_WaitGroup.
+func ExampleWaitGroup_justErrors() {
+	wg, ctx := sync.New(context.Background(), 2)
+
+	urls := []string{
+		"http://www.example.org/",
+		"http://www.example.com/",
+		"http://www.nonExistentDomainName.com/",
+	}
+
+	funcs := []func() error{}
+	for _, url := range urls {
+		url := url // https://golang.org/doc/faq#closures_and_goroutines
+		funcs = append(
+			funcs,
+			func() error {
+				// Fetch the URL.
+				ctx, cancel := context.WithTimeout(ctx, 4*time.Second)
+				defer cancel()
+
+				req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+				if err != nil {
+					return err
+				}
+				resp, err := http.DefaultClient.Do(req)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "url: %v. resp: %v", url, err)
+					return err
+				}
+				defer resp.Body.Close()
+				fmt.Fprintf(os.Stderr, "url: %v. resp: %v", url, resp.StatusCode)
+				return err
+			},
+		)
+	}
+
+	err := wg.Go(funcs...)
+	fmt.Printf("\n\t err: %v. cause: %v\n\n", err, context.Cause(ctx))
+
+	// Output:
+	// err: Get "http://www.nonExistentDomainName.com/": context deadline exceeded. cause: Get "http://www.nonExistentDomainName.com/": context deadline exceeded
+}
