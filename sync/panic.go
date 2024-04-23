10e21e67e84d1aec5021233794ba813e1c89d00d
komuw <komuw05@gmail.com>
2024-04-23 16:09:28 +0300

    m

--- sync/panic.go
+++ sync/panic.go
@@ -7,7 +7,6 @@ import (
 )
 
 // TODO: docs
-// TODO: remove mention of errgroup here.
 
 // A PanicError wraps an error recovered from an unhandled panic
 // when calling a function passed to Go or TryGo.
@@ -19,9 +18,9 @@ type PanicError struct {
 func (p PanicError) Error() string {
 	// A Go Error method conventionally does not include a stack dump, but here we add it.
 	if len(p.Stack) > 0 {
-		return fmt.Sprintf("recovered from errgroup.Group: %v\n%s", p.Recovered, p.Stack)
+		return fmt.Sprintf("recovered from Group: %v\n%s", p.Recovered, p.Stack)
 	}
-	return fmt.Sprintf("recovered from errgroup.Group: %v", p.Recovered)
+	return fmt.Sprintf("recovered from Group: %v", p.Recovered)
 }
 
 func (p PanicError) Unwrap() error { return p.Recovered }
@@ -36,9 +35,9 @@ type PanicValue struct {
 
 func (p PanicValue) String() string {
 	if len(p.Stack) > 0 {
-		return fmt.Sprintf("recovered from errgroup.Group: %v\n%s", p.Recovered, p.Stack)
+		return fmt.Sprintf("recovered from Group: %v\n%s", p.Recovered, p.Stack)
 	}
-	return fmt.Sprintf("recovered from errgroup.Group: %v", p.Recovered)
+	return fmt.Sprintf("recovered from Group: %v", p.Recovered)
 }
 
 // addStack returns a PanicError or PanicValue that wraps v with a stack trace
