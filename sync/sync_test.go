fad04f29a8b40a7630778a3b66fbc1e6c3d2e856
komuw <komuw05@gmail.com>
2024-04-24 22:15:47 +0300

    m

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -428,7 +428,7 @@ func TestPanic(t *testing.T) {
 			attest.True(t, ok)
 			gotStr := fmt.Sprintf("%+#s", got)
 			attest.Subsequence(t, gotStr, "hey hey")          // The panic message
-			attest.Subsequence(t, gotStr, "sync_test.go:329") // The place where the panic happened
+			attest.Subsequence(t, gotStr, "sync_test.go:423") // The place where the panic happened
 		}
 	})
 
@@ -450,7 +450,7 @@ func TestPanic(t *testing.T) {
 			attest.True(t, ok)
 			gotStr := val.Error()
 			attest.Subsequence(t, gotStr, errPanic.Error())   // The panic message
-			attest.Subsequence(t, gotStr, "sync_test.go:351") // The place where the panic happened
+			attest.Subsequence(t, gotStr, "sync_test.go:445") // The place where the panic happened
 		}
 	})
 }
