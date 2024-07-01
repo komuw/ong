edfe156903d56c7346abfa09ea2ae72ff472ac99
komuw <komuw05@gmail.com>
2024-07-01 10:37:06 +0300

    update

--- errors/stdlib_test.go
+++ errors/stdlib_test.go
@@ -19,8 +19,8 @@ func TestStdLib(t *testing.T) {
 
 		_, ok := err.(*stackError)
 		attest.True(t, ok)
+		attest.True(t, As(err, &targetErr))
 		attest.True(t, Is(err, os.ErrNotExist))
 		attest.NotZero(t, Unwrap(err))
-		attest.True(t, As(err, &targetErr))
 	})
 }
